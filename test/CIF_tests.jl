e732828bf6ff6c0692fd8b28151fed25fc28285e
Rachel Kurchin <rkurchin@cmu.edu>
2020-08-19 16:20:09 -0400

    fix relative path in tests

--- test/CIF_tests.jl
+++ test/CIF_tests.jl
@@ -60,11 +60,11 @@ end
 end
 
 @testset "graph-building" begin
-    wm, atoms = build_graph("./test_data/mp-195.cif")
+wm, atoms = build_graph(joinpath(@__DIR__, "./test_data/mp-195.cif")
     wm_true = [0.0 1.0 1.0 1.0; 1.0 0.0 1.0 1.0; 1.0 1.0 0.0 1.0; 1.0 1.0 1.0 0.0]
     @test wm == wm_true
     @test atoms == ["Ho", "Pt", "Pt", "Pt"]
-    wm, atoms = build_graph("./test_data/mp-195.cif"; use_voronoi=false)
+    wm, atoms = build_graph(joinpath(@__DIR__, "./test_data/mp-195.cif"; use_voronoi=false)
     @test wm == wm_true
     @test atoms == ["Ho", "Pt", "Pt", "Pt"]
 end
