86a4997b539759c15fb910f7cb028c90aaaefcf4
Anant Thazhemadam <anant.thazhemadam@gmail.com>
2021-03-07 00:05:37 +0530

    docs: set up documentation hosting with GitHub Actions
    Set up CI workflow for publishing the documentation to a github.io page.
    
    Signed-off-by: Anant Thazhemadam <anant.thazhemadam@gmail.com>

--- docs/make.jl
+++ docs/make.jl
@@ -6,17 +6,10 @@ makedocs(
 	pages = Any[
 		"Home" => "index.md",
 		"Terminology" => "terminology.md",
-		"Autodocs" => "autodocs.md"
 	]
 )
 deploydocs(
-    repo = "github.com/thazhemadam/ChemistryFeaturization.jl.git",
-     target = "build"
+	repo = "github.com/thazhemadam/ChemistryFeaturization.jl.git",
+	target = "build",
+	branch = "gh-pages"
 )
-
-# deploydocs(
-	# repo = "github.com/thazhemadam/ChemistryFeaturization.jl.git",
-	# target = "build",
-	# push_preview = true,
-	# branch = "gh-pages"
-# )
