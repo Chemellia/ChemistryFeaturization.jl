Chemellia/ChemistryFeaturization.jl

commit fdd29b5e48e9de1076450a4144285df1e8f90dc0

add option to store id in AtomGraph, clean up unused functions, redo build_graphs_batch, fix add_features_batch, fix deprecation warning...this probably should have been a couple separate commits, oops

src/atomfeat.jl
@@ -8,7 +8,7 @@ const default_nbins = 10
 
 # read in features...
 atom_data_path = joinpath(dirname(pathof(ChemistryFeaturization)), "..", "data", "pymatgen_atom_data.csv")
-const atom_data_df = DataFrame!(CSV.File(atom_data_path))
+const atom_data_df = DataFrame(CSV.File(atom_data_path))
 feature_info_path = joinpath(dirname(pathof(ChemistryFeaturization)), "..", "data", "feature_info.json")
 const feature_info = JSON.parsefile(feature_info_path)
 

src/atomgraph.jl
@@ -8,17 +8,17 @@ include("pmg_graphs.jl")
 
 # Type to store atomic graphs
 # TO CONSIDER: store ref to featurization rather than the thing itself? Does this matter for any performance we care about?
-# TO CONSIDER: store an ID of some kind? (e.g. mp-123 or whatever)
 mutable struct AtomGraph <: lg.AbstractGraph{Float32}
     graph::SimpleWeightedGraph{Int32,Float32}
     elements::Vector{String} # list of elemental symbols corresponding to each node
     lapl::Matrix{Float32} # graph laplacian (normalized)
     features::Matrix{Float32} # feature matrix (size (# features, # nodes))
     featurization::Vector{AtomFeat} # featurization scheme in the form of a list of AtomFeat objects
+    id::String # optional, id for cross-checking with databases, etc.
 end
 
 # basic constructor
-function AtomGraph(gr::SimpleWeightedGraph{Int32,Float32}, el_list::Vector{String}, features::Matrix{Float32}, featurization::Vector{AtomFeat})
+function AtomGraph(gr::SimpleWeightedGraph{Int32,Float32}, el_list::Vector{String}, features::Matrix{Float32}, featurization::Vector{AtomFeat}, id="")
     # check that el_list is the right length
     num_atoms = size(gr)[1]
     @assert length(el_list)==num_atoms "Element list length doesn't match graph size!"
@@ -29,22 +29,23 @@ function AtomGraph(gr::SimpleWeightedGraph{Int32,Float32}, el_list::Vector{Strin
 
     # if all these are good, calculate laplacian and build the thing
     lapl = normalized_laplacian(gr)
-    AtomGraph(gr, el_list, lapl, features, featurization)
+    AtomGraph(gr, el_list, lapl, features, featurization, id)
 end
 
 # one without features or featurization initialized yet
-function AtomGraph(gr::SimpleWeightedGraph{Int32,Float32}, el_list::Vector{String})
+function AtomGraph(gr::SimpleWeightedGraph{Int32,Float32}, el_list::Vector{String}, id="")
     # check that el_list is the right length
     num_atoms = size(gr)[1]
     @assert length(el_list)==num_atoms "Element list length doesn't match graph size!"
 
     lapl = Float32.(normalized_laplacian(gr))
-    AtomGraph(gr, el_list, lapl, zeros(Float32, 1, num_atoms), AtomFeat[])
+    AtomGraph(gr, el_list, lapl, zeros(Float32, 1, num_atoms), AtomFeat[], id)
 end
 
-# TODO, maybe: constructor where you give adjacency matrix and it builds the graph for you also
+# initialize directly from adjacency matrix
+AtomGraph(adj::Array{Float32}, el_list::Vector{String}, features::Matrix{Float32}, featurization::Vector{AtomFeat}, id="") = AtomGraph(SimpleWeightedGraph{Int32}(adj), el_list, features, featurization, id)
+AtomGraph(adj::Array{Float32}, el_list::Vector{String}, id="") = AtomGraph(SimpleWeightedGraph{Int32}(adj), el_list, id)
 
-# TODO: fix so that it will print feature length even if featurization isn't initialized
 # pretty printing, short version
 function Base.show(io::IO, g::AtomGraph)
     st = "AtomGraph with $(nv(g)) nodes, $(ne(g)) edges"
@@ -133,12 +134,6 @@ function add_features!(g::AtomGraph, feature_names::Vector{Symbol}, nbins::Vecto
 end
 
 # and the batch versions
-function add_features_batch!(gs::Array{AtomGraph}, features, featurization)
-    for g in gs
-        add_features!(g, features, featurization)
-    end
-end
-
 function add_features_batch!(gs::Array{AtomGraph}, atom_feature_vecs::Dict{String, Vector{Float32}}, featurization::Vector{AtomFeat})
     for g in gs
         add_features!(g, atom_feature_vecs, featurization)
@@ -146,7 +141,7 @@ function add_features_batch!(gs::Array{AtomGraph}, atom_feature_vecs::Dict{Strin
 end
 
 function add_features_batch!(gs::Array{AtomGraph}, featurization::Vector{AtomFeat})
-    feature_vecs, featurization = make_feature_vectors(feature_names)
+    feature_vecs, featurization = make_feature_vectors(featurization)
     add_features_batch!(gs, feature_vecs, featurization)
 end
 

src/pmg_graphs.jl
@@ -7,38 +7,12 @@ using ChemistryFeaturization
 using Serialization
 
 # options for decay of bond weights with distance...
+# user can of course write their own as well
 inverse_square(x) = x^-2.0
 exp_decay(x) = exp(-x)
 
-# a few helper fcns for readability...
-"Return the index of a given site in the structure."
-site_index(site) = convert(UInt16, get(site, 2)) + 1
-
-"Return the distance associated with a site in a neighbor list."
-site_distance(site) = convert(Float64, get(site, 1))
-
-# these next two functions return the information for the first species in a list – there should only be one because otherwise the structure would be disordered and we probably shouldn't be building a graph...(or maybe we add some fancy functionality later to do superpositions of species?)
-
-"Return atomic number associated with a site."
-site_atno(site) = [e.Z for e in site.species.elements][1]
-
-"Return atomic symbol associated with a site."
-site_element(site) = [e.symbol for e in site.species.elements][1]
-
-
-"""
-    are_equidistant(site1, site2)
-
-Check if site1 and site2 are equidistant to within tolerance atol, in angstroms (for cutting off neighbor lists consistently).
-
-Note that this only works if site1 and site2 are from a neighbor list from the same central atom.
-"""
-function are_equidistant(site1, site2, atol=1e-4)
-    isapprox(site_distance(site1), site_distance(site2), atol=atol)
-end
-
-
 # TODO: figure out best/idiomatic way to pass through the keyword arguments, surely the copy/paste is not it
+# TODO: option to featurize here?
 """
 Function to build graph from a file storing a crystal structure (currently supports anything ase.io.read can read in). Returns an AtomGraph object.
 
@@ -52,8 +26,6 @@ Function to build graph from a file storing a crystal structure (currently suppo
 - `max_num_nbr::Integer=12`: maximum number of neighbors to include (even if more fall within cutoff radius)
 - `dist_decay_func`: function (e.g. inverse_square or exp_decay) to determine falloff of graph edge weights with neighbor distance
 """
-
-# TODO: featurize here
 function build_graph(file_path::String; use_voronoi=false, radius=8.0, max_num_nbr=12, dist_decay_func=inverse_square, normalize=true)
     # see if this fixes issues on Windows
     aseio = pyimport_conda("ase.io", "ase", "conda-forge")
@@ -149,41 +121,45 @@ function weights_cutoff(is, js, dists; max_num_nbr=12, dist_decay_func=inverse_s
     weight_mat = 0.5 .* (weight_mat .+ weight_mat')
 end
 
+# TODO: might want a version that only serializes for cases where full array of AtomGraphs won't fit in memory?
 """
-Function to build and serialize to file a batch of CIFs, optionally featurizing them as well. Saved .jls files will have the same names as the .cif ones but with the extensions modified.
+Function to build and, optionally, serialize to file a batch of input files, optionally featurizing them as well. Saved .jls files will have the same names as the .cif (or .traj, or .xyz, or ...) ones but with the extensions modified.
 
 # Arguments
-- `cif_folder::String`: path to folder containing CIF files
-- `output_folder::String`: path to folder where .jls files containing AtomGraph objects should be saved (will be created if it doesn't exist already)
+- `input_folder::String`: path to folder containing CIF files
+- `output_folder::String` (optional): path to folder where .jls files containing AtomGraph objects should be saved (will be created if it doesn't exist already)
 
 If you want to featurize the graphs, at least the vector of `AtomFeat` objects describing the featurization procedure is required, and optionally the Dict mapping from elemental symbols to feature vectors (if it is not provided, it will be generated).
 
 Other optional arguments are the optional arguments to `build_graph`: `use_voronoi`, `radius`, `max_num_nbr`, `dist_decay_func`, `normalize`
-
-This function does not return anything.
 """
-function build_graphs_batch(input_folder::String, output_folder::String, featurization=AtomFeat[]; atom_featurevecs=Dict{String, Vector{Float32}}(), use_voronoi=false, radius=8.0, max_num_nbr=12, dist_decay_func=inverse_square, normalize=true)
+function build_graphs_batch(input_folder::String, featurization=AtomFeat[]; atom_featurevecs=Dict{String, Vector{Float32}}(), use_voronoi=false, radius=8.0, max_num_nbr=12, dist_decay_func=inverse_square, normalize=true, output_folder="")
     # check if input folder exists and contains things, if not throw error
+    println(input_folder)
     file_list = readdir(input_folder, join=true)
-    if length(file_list)==0
-        error("No files in input directory!")
-    end
+    println(file_list)
+    length(file_list)!=0 || throw(ArgumentError("No files in input directory!"))
 
     # check if output folder exists, if not create it
-    if !isdir(output_folder)
-        mkpath(output_folder)
-        @info "Output path provided did not exist, creating folder there."
+    to_serialize = false
+    if output_folder != ""
+        to_serialize=true
+        if !isdir(output_folder)
+            mkpath(output_folder)
+            @info "Output path provided did not exist, creating folder there."
+        end
     end
 
     # check if there is enough information to actually featurize
-    featurize = length(featurization)>0
-    atom_featurevecs = (length(atom_featurevecs)>0 & featurize) ? atom_featurevecs : make_feature_vectors(featurization)[1]
+    to_featurize = length(featurization)>0
+    atom_featurevecs = (length(atom_featurevecs)>0 & to_featurize) ? atom_featurevecs : make_feature_vectors(featurization)[1]
 
     if (length(atom_featurevecs)>0) & (length(featurization)==0)
         @warn "You have supplied only feature vectors but no featurization scheme, so graphs will be built but not featurized."
     end
 
     # loop over CIFs and build graphs from all files that we can...
+    graphs = AtomGraph[]
     for file in file_list
         id = split(splitpath(file)[end], ".")[1]
         local ag
@@ -193,16 +169,20 @@ function build_graphs_batch(input_folder::String, output_folder::String, featuri
             @warn "Unable to build graph for $file"
             continue
         end
-        if featurize
+        if to_featurize
             add_features!(ag, atom_featurevecs, featurization)
         end
-        graph_path = joinpath(output_folder, string(id, ".jls"))
-        serialize(graph_path, ag)
+        if to_serialize
+            graph_path = joinpath(output_folder, string(id, ".jls"))
+            serialize(graph_path, ag)
+        end
+        push!(graphs, ag)
     end
+    return graphs
 end
 
 # alternate call signature where featurization is generated
-function build_graphs_batch(cif_folder::String, output_folder::String, feature_names::Vector{Symbol}; nbins::Vector{<:Integer}=default_nbins*ones(Int64, size(feature_names,1)), logspaced=false)
+function build_graphs_batch(cif_folder::String, feature_names::Vector{Symbol}; nbins::Vector{<:Integer}=default_nbins*ones(Int64, size(feature_names,1)), logspaced=false, output_folder="")
     atom_featurevecs, featurization = make_feature_vectors(build_atom_feats(feature_names; nbins=nbins, logspaced=logspaced))
     build_graphs_batch(cif_folder, output_folder; featurization=featurization, atom_featurevecs = atom_featurevecs)
 end

test/atomgraph_tests.jl
@@ -6,13 +6,16 @@ using ChemistryFeaturization
 
 @testset "AtomGraph" begin
     # build a silly little triangle graph
-    g = SimpleWeightedGraph{Int32}(Float32.([0 1 1; 1 0 1; 1 1 0]))
+    adj = Float32.([0 1 1; 1 0 1; 1 1 0])
+    g = SimpleWeightedGraph{Int32}(adj)
 
     # add an element list that doesn't make sense
     @test_throws AssertionError AtomGraph(g, ["C"])
 
-    # okay, now do it right, start with no features
+    # okay, now do it right, start with no features, checking both constructors
     ag = AtomGraph(g, ["C", "C", "C"])
+    ag2 = AtomGraph(adj, ["C", "C", "C"])
+    @test adjacency_matrix(ag)==adjacency_matrix(ag2)
 
     # check LightGraphs fcns
     @test eltype(ag)==Int32
@@ -33,14 +36,6 @@ using ChemistryFeaturization
     @test_throws AssertionError add_features!(ag, bad_fmat, featurization)
     add_features!(ag, good_fmat, featurization)
     @test ag.features==good_fmat
-
-    # test that we can add features to multiple graphs
-    ag = AtomGraph(g, ["C", "C", "C"])
-    ag2 = deepcopy(ag)
-    ags = [ag ag2]
-    add_features_batch!(ags, good_fmat, featurization)
-    @test ag.features==good_fmat
-    @test ag2.features==good_fmat
     
     # tests for other signatures of add_features! where feature vectors are built automatically
     ag = AtomGraph(g, ["C", "C", "C"])
@@ -54,6 +49,19 @@ using ChemistryFeaturization
     add_features!(ag3, feature_names, Int32.([4,3]))
 
     @test ag.features==ag2.features==ag3.features==Float32.(hcat([vecs["C"] for i in 1:3]...))
+
+    # test that we can add features to multiple graphs
+    ag = AtomGraph(g, ["C", "C", "C"])
+    ag2 = deepcopy(ag)
+    ags = [ag ag2]
+    add_features_batch!(ags, featurization)
+    
+    # and test this one for the other signatures too
+    add_features_batch!(ags, vecs, featurization)
+    @test ags[1].features[:,1]==Float32.([0;1;0;0;0;1;0])==ags[2].features[:,1]    
+    # sneakily test that block will always be length 4, i.e. second entry ignored
+    add_features_batch!(ags, Symbol.(["X", "Block"]), [3,3])
+    @test ags[1].features[:,2]==Float32.([0;1;0;0;1;0;0])==ags[2].features[:,2]
 end
 
 @testset "graph-building" begin
@@ -83,7 +91,6 @@ end
 
     # test that warning is thrown for NaNs in laplacian
     @test_throws ArgumentError build_graph(joinpath(@__DIR__, "test_data", "nanlaplstruc.cif"))
-
 end
 
 @testset "save/load" begin
@@ -106,7 +113,7 @@ end
 
 @testset "batch processing" begin
     featurization = build_atom_feats([Symbol("Atomic mass"), :Block])
-    build_graphs_batch(joinpath(@__DIR__, "test_data"), joinpath(@__DIR__, "test_data", "graphs"), featurization)
+    gs = build_graphs_batch(joinpath(@__DIR__, "test_data"), featurization, output_folder=joinpath(@__DIR__, "test_data", "graphs"))
     
     g1 = deserialize(joinpath(@__DIR__, "test_data","graphs","mp-195.jls"))
     @test size(g1)==(4,4)

commit 213931f9ef559f26ea87c3f7fd5be8fa2968ef1b

src: atomfeat.jl: fix deprecation warning correctly
Commit fdd29b5 mentions how it fixes some deprecation warnings.
However, this deprecation warning wasn't correctly fixed.

Previously, compiling the code produced the following warning:

┌ Warning: `DataFrame!(args...; kwargs...)` is deprecated, use `DataFrame(args...; copycols = false, kwargs...)` instead.
│   caller = top-level scope at atomfeat.jl:11
└ @ Core ~/deepchem/ChemistryFeaturization.jl/src/atomfeat.jl:11

Seeing how the deprecation warning fix applied doesn't do what was
suggested, this commit fixes the deprecation warning correctly (in line
with what was suggested by the warning).

Fixes: fdd29b5
Signed-off-by: Anant Thazhemadam <anant.thazhemadam@gmail.com>

src/atomfeat.jl
@@ -8,7 +8,7 @@ const default_nbins = 10
 
 # read in features...
 atom_data_path = joinpath(dirname(pathof(ChemistryFeaturization)), "..", "data", "pymatgen_atom_data.csv")
-const atom_data_df = DataFrame(CSV.File(atom_data_path))
+const atom_data_df = DataFrame(CSV.File(atom_data_path); copycols = false)
 feature_info_path = joinpath(dirname(pathof(ChemistryFeaturization)), "..", "data", "feature_info.json")
 const feature_info = JSON.parsefile(feature_info_path)
 