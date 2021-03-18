Chemellia/ChemistryFeaturization.jl

commit a441cf7b0a1111df59af997243f9899b6a0ab7bd

src: pmg_graphs: add option for overwrites
Addresses #45.

Add option for overwriting in `build_graphs_batch()` (default value of
overwrite = true).

If `overwrite = false`, first check if the output directory exists,
and create a new list (`existing_list`) that contains names of all the files
in the output directory. While building the graphs, cross-reference with
`existing_list` to  check if there already exists a file with the same
name (which means this graph exists and was previously built), and continue
execution accordingly.

Signed-off-by: Anant Thazhemadam <anant.thazhemadam@gmail.com>

src/pmg_graphs.jl
@@ -140,12 +140,17 @@ Other optional keyword arguments are the optional arguments to `build_graph`: `u
 
 See also: [`build_graph`](@ref)
 """
-function build_graphs_batch(input_folder::String, featurization=AtomFeat[]; atom_featurevecs=Dict{String, Vector{Float32}}(), use_voronoi=false, radius=8.0, max_num_nbr=12, dist_decay_func=inverse_square, normalize=true, output_folder="")
+function build_graphs_batch(input_folder::String, featurization=AtomFeat[]; atom_featurevecs=Dict{String, Vector{Float32}}(), use_voronoi=false, radius=8.0, max_num_nbr=12, dist_decay_func=inverse_square, normalize=true, output_folder="", overwrite=true)
 
     file_list = filter((file) -> isfile(file), readdir(input_folder, join = true))
 
     # check if input folder exists and contains things, if not throw error
     length(file_list)!=0 || throw(ArgumentError("No files in input directory!"))
+
+    if(!overwrite && isdir(output_folder))
+        existing_files = filter((file) -> isfile(file), readdir(output_folder, join = true))
+        existing_files = map(file -> string(split(splitpath(file)[end], ".")[1]), existing_files)
+    end
 
     # check if output folder exists, if not create it
     to_serialize = false
@@ -168,7 +173,14 @@ function build_graphs_batch(input_folder::String, featurization=AtomFeat[]; atom
     # loop over CIFs and build graphs from all files that we can...
     graphs = AtomGraph[]
     for file in file_list
+
         id = split(splitpath(file)[end], ".")[1]
+
+        if (!overwrite) && isdir(output_folder) && (id in existing_files)
+            @info "Graph for $id already exists. Skipping, not overwriting."
+            continue
+        end
+
         local ag
         try 
             ag = build_graph(file; use_voronoi=use_voronoi, radius=radius, max_num_nbr=max_num_nbr, dist_decay_func=dist_decay_func, normalize=normalize)
@@ -200,9 +212,9 @@ Alternative version of this function that builds the featurization from a list o
 
 Other optional keyword arguments are those for [`build_graph`](@ref) as well as for [`build_featurization`](@ref)
 """
-function build_graphs_batch(input_folder::String, feature_names::Vector{Symbol}; nbins::Vector{<:Integer}=default_nbins*ones(Int64, size(feature_names,1)), logspaced=false, use_voronoi=false, radius=8.0, max_num_nbr=12, dist_decay_func=inverse_square, normalize=true, output_folder="")
+function build_graphs_batch(input_folder::String, feature_names::Vector{Symbol}; nbins::Vector{<:Integer}=default_nbins*ones(Int64, size(feature_names,1)), logspaced=false, use_voronoi=false, radius=8.0, max_num_nbr=12, dist_decay_func=inverse_square, normalize=true, output_folder="", overwrite=true)
     atom_featurevecs, featurization = make_feature_vectors(build_featurization(feature_names; nbins=nbins, logspaced=logspaced))
-    build_graphs_batch(input_folder, featurization; atom_featurevecs = atom_featurevecs, use_voronoi=use_voronoi, radius=radius, max_num_nbr=max_num_nbr, dist_decay_func=dist_decay_func, normalize=normalize, output_folder=output_folder)
+    build_graphs_batch(input_folder, featurization; atom_featurevecs = atom_featurevecs, use_voronoi=use_voronoi, radius=radius, max_num_nbr=max_num_nbr, dist_decay_func=dist_decay_func, normalize=normalize, output_folder=output_folder, overwrite=overwrite)
 end
 
 # function to read in graphs generated by `build_graphs_batch` to an Array of AtomGraph objects

commit 85cbffed251195edc0e3ed91e0b5aef7baa35fea

src: pmg_graphs: fix overwrite condition
Consider the case where overwrite=false, and output_directory doesn't
exist. In this case, !overwrite is true, and isdir(output_directory) is
true as well (since we create an output_directory if it doesn't exist
already).
However, the check (id in existing_files) gives an error as existing_files
isn't defined at this point (existing_files is defined only when !overwrite
and isdir(output_directory)). So, an undefined variable is illegally
accessed.
Fix this issue by increasing the scope of existing_files (by one level)
and updating the check to see if there are existing_files that will need
to be skipped, and not overwritten.

Fixes: a441cf7 ("src: pmg_graphs: add option for overwrites")
Signed-off-by: Anant Thazhemadam <anant.thazhemadam@gmail.com>

src/pmg_graphs.jl
@@ -147,6 +147,7 @@ function build_graphs_batch(input_folder::String, featurization=AtomFeat[]; atom
     # check if input folder exists and contains things, if not throw error
     length(file_list)!=0 || throw(ArgumentError("No files in input directory!"))
 
+    local existing_files = []
     if(!overwrite && isdir(output_folder))
         existing_files = filter((file) -> isfile(file), readdir(output_folder, join = true))
         existing_files = map(file -> string(split(splitpath(file)[end], ".")[1]), existing_files)
@@ -176,7 +177,7 @@ function build_graphs_batch(input_folder::String, featurization=AtomFeat[]; atom
 
         id = split(splitpath(file)[end], ".")[1]
 
-        if (!overwrite) && isdir(output_folder) && (id in existing_files)
+        if (!overwrite) && (length(existing_files) != 0) && (id in existing_files)
             @info "Graph for $id already exists. Skipping, not overwriting."
             continue
         end

test/atomgraph_tests.jl
@@ -58,7 +58,7 @@ using ChemistryFeaturization
     
     # and test this one for the other signatures too
     add_features_batch!(ags, vecs, featurization)
-    @test ags[1].features[:,1]==Float32.([0;1;0;0;0;1;0])==ags[2].features[:,1]    
+    @test ags[1].features[:,1]==Float32.([0;1;0;0;0;1;0])==ags[2].features[:,1]
     # sneakily test that block will always be length 4, i.e. second entry ignored
     add_features_batch!(ags, Symbol.(["X", "Block"]); nbins=[3,3])
     @test ags[1].features[:,2]==Float32.([0;1;0;0;1;0;0])==ags[2].features[:,2]
@@ -117,8 +117,9 @@ end
     input_folder = joinpath(@__DIR__, "test_data")
     output_folder=joinpath(@__DIR__, "test_data", "graphs")
 
-    gs = build_graphs_batch(input_folder, featurization, output_folder=output_folder)
-    gs2 = build_graphs_batch(input_folder, feature_names)
+    # sneakily run the case wherein output_folder doesn't exist, and overwrite is set to false
+    gs = build_graphs_batch(input_folder, featurization, output_folder = output_folder)
+    gs2 = build_graphs_batch(input_folder, feature_names, overwrite = true)
     @test repr.(gs)==repr.(gs2) # sneakily testing pretty printing also...
 
     # test reading from individual files