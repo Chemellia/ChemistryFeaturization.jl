16e3999d578cb1491830ef82f563248fc1b6c623
Rachel Kurchin <rkurchin@cmu.edu>
2021-03-22 19:56:23 -0400

    add changelog

--- docs/make.jl
+++ docs/make.jl
@@ -1,18 +1,19 @@
 using Documenter, ChemistryFeaturization
 
 makedocs(
-	sitename = "ChemistryFeaturization.jl",
-	modules = [ChemistryFeaturization],
-	pages = Any[
-		"Home" => "index.md",
-		"Terminology" => "terminology.md",
-		"AtomFeat" => "types/atomfeat.md",
-		"AtomGraph" => "types/atomgraph.md",
-		"Building Atomic Graphs" => "build_ag.md",
+    sitename = "ChemistryFeaturization.jl",
+    modules = [ChemistryFeaturization],
+    pages = Any[
+	"Home" => "index.md",
+	"Terminology" => "terminology.md",
+	"AtomFeat" => "types/atomfeat.md",
+	"AtomGraph" => "types/atomgraph.md",
+	"Building Atomic Graphs" => "build_ag.md",
+        "Changelog" => "changelog.md",    
 	]
 )
 deploydocs(
-	repo = "github.com/thazhemadam/ChemistryFeaturization.jl.git",
+	repo = "github.com/aced-differentiate/ChemistryFeaturization.jl.git",
         target = "build",
         devbranch = "main",
 	branch = "gh-pages"
---
+++ docs/src/changelog.md
@@ -0,0 +1,14 @@
+# Changelog
+
+## Upcoming
+* major restructure to make extensibility far easier and allow better sharing of elements between featurization schemes!
+
+## v0.2.2 [2021-02-22]
+
+## v0.2.1 [2021-02-22]
+
+## v0.2.0 [2021-02-16]
+
+## v0.1.1 [2021-02-10]
+
+## v0.1.0 [2020-12-22]
