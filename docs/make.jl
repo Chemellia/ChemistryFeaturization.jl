4fa7d5a558eb75a4f23cb2d39de67859500ad593
Rachel Kurchin <rkurchin@cmu.edu>
2021-03-22 19:52:32 -0400

    fix the branch names

--- docs/make.jl
+++ docs/make.jl
@@ -22,6 +22,7 @@ makedocs(
 )
 deploydocs(
 	repo = "github.com/thazhemadam/ChemistryFeaturization.jl.git",
-	target = "build",
+        target = "build",
+        devbranch = "main",
 	branch = "gh-pages"
 )
