78771681a9b21ca5dc395f185d72d17becd14686
Rachel Kurchin <rkurchin@cmu.edu>
2021-04-09 11:58:41 -0400

    rewrite generic featurize! function

--- src/featurizations/Featurizations.jl
+++ src/featurizations/Featurizations.jl
@@ -4,6 +4,8 @@ All such objects should define at least one list of <:AbstractFeature objects an
 
 module Featurizations
 
+using Functors
+
 # include...
 include("graphnodefeaturization.jl")
 include("weavefeaturization.jl")
@@ -11,21 +13,22 @@ include("weavefeaturization.jl")
 # export...
 
 
-# maybe also define abstract type here
+
 abstract type AbstractFeaturization end
 
-# generic featurize...there's probably a better way to write this...
-# TODO: rewrite Dhairya's way
+# generic featurize!
+# this assumes that `a` has fields with names corresponding to each field in `fzn`, if not you need to dispatch this function to your specific case
+# TODO: maybe add option to exclude field names from iteration over fzn?
 # docstring
-function featurize!(a<:AbstractAtoms, f<:AbstractFeaturization)
-    feats_lists = [field for field in fieldnames(typeof(f)) if field!==:combine]
-    encoded_features = Dict(feats_list => [] for feats_list in feats_lists)
-    for feats_list in feats_lists
-        features = map(feat->feat(a), feats_list)
-        encoded_features[feats_list] = features
+function featurize!(a<:AbstractAtoms, fzn<:AbstractFeaturization)
+    # loop over fields in featurization, each one is a list of features
+    # encode each feature in that list and assign the results to the
+    # field of the same name in `a`
+    for feats_list in fieldnames(fzn)
+        encoded = reduce(vcat, map((x)->x(a), feats_list))
+        setproperty!(a, feats_list, encoded)
     end
-    a.features = f.combine(encoded_features)
-    a.featurization = f
+    a.featurization = fzn
 end
 
 end
