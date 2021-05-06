Chemellia/ChemistryFeaturization.jl

commit 513c520fee9ba28a3788e0597d44e136289e72d2

set up JuliaFormatter

src/atoms/atomgraph.jl
@@ -46,10 +46,16 @@ representing each node. Note that the object can be initialized without features
 features are provided, so too must be the featurization scheme, in order to maintain
 "decodability" of features.
 """
-function AtomGraph(gr::SimpleWeightedGraph{A,B}, el_list::Vector{String}, features::Matrix{<:Real}, featurization::AbstractFeaturization, id="") where {B<:Real,A<:Integer}
+function AtomGraph(
+    gr::SimpleWeightedGraph{A,B},
+    el_list::Vector{String},
+    features::Matrix{<:Real},
+    featurization::AbstractFeaturization,
+    id = "",
+) where {B<:Real,A<:Integer}
     # check that el_list is the right length
     num_atoms = size(gr)[1]
-    @assert length(el_list)==num_atoms "Element list length doesn't match graph size!"
+    @assert length(el_list) == num_atoms "Element list length doesn't match graph size!"
 
     # check that features is the right dimensions (# features x # nodes)
     expected_feature_length = sum(f.num_bins for f in featurization)
@@ -63,26 +69,39 @@ end
 # one without features or featurization initialized yet
 function AtomGraph(gr::SimpleWeightedGraph{A,B}, el_list::Vector{String}, id="") where {B<:Real,A<:Integer}
     # check that el_list is the right length
+    AtomGraph(gr, el_list, lapl, features, featurization, id)
+end
+
+# one without features or featurization initialized yet
+function AtomGraph(
+    gr::SimpleWeightedGraph{A,B},
+    el_list::Vector{String},
+    id = "",
+) where {B<:Real,A<:Integer}
+    # check that el_list is the right length
     num_atoms = size(gr)[1]
-    @assert length(el_list)==num_atoms "Element list length doesn't match graph size!"
+    @assert length(el_list) == num_atoms "Element list length doesn't match graph size!"
 
     lapl = B.(normalized_laplacian(gr))
     AtomGraph(gr, el_list, lapl, nothing, nothing, id)
 end
 
 # initialize directly from adjacency matrix, defaults to 32-bit integers because unlikely to need more nodes than that...
-AtomGraph(adj::Array{R}, el_list::Vector{String}, features::Matrix{R}, featurization::AbstractFeaturization, id=""; U=UInt32) where {R<:Real} = AtomGraph(SimpleWeightedGraph{R}(adj), el_list, features, featurization, id)
-AtomGraph(adj::Array{R}, el_list::Vector{String}, id=""; U=UInt32) where {R<:Real} = AtomGraph(SimpleWeightedGraph{R}(adj), el_list, id)
+AtomGraph(
+    adj::Array{R},
+    el_list::Vector{String},
+    features::Matrix{R},
+    featurization::AbstractFeaturization,
+    id = "";
+    U = UInt32,
+) where {R<:Real} =
+    AtomGraph(SimpleWeightedGraph{R}(adj), el_list, features, featurization, id)
+AtomGraph(adj::Array{R}, el_list::Vector{String}, id = ""; U = UInt32) where {R<:Real} =
+    AtomGraph(SimpleWeightedGraph{R}(adj), el_list, id)
 
 # pretty printing, short version
 function Base.show(io::IO, ag::AtomGraph)
     st = "AtomGraph $(ag.id) with $(nv(ag.graph)) nodes, $(ne(ag.graph)) edges"
-    if !isnothing(ag.featurization)
-        st = string(st, ", feature vector length $(size(ag.atom_feats)[1])")
-    end
-    print(io, st)
-end
-
 # pretty printing, long version
 function Base.show(io::IO, ::MIME"text/plain", ag::AtomGraph)
     st = "AtomGraph $(ag.id) with $(nv(ag.graph)) nodes, $(ne(ag.graph)) edges\n   atoms: $(ag.elements)\n   feature vector length: "
@@ -104,14 +123,18 @@ Compute the normalized graph Laplacian matrix of the input graph, defined as
 where ``A`` is the adjacency matrix and ``D`` is the degree matrix.
 """
 function normalized_laplacian(g::G) where G<:LightGraphs.AbstractGraph
-    a = adjacency_matrix(g)
-    d = vec(sum(a, dims=1))
-    inv_sqrt_d = diagm(0=>d.^(-0.5f0))
-    lapl = Float32.(I - inv_sqrt_d * a * inv_sqrt_d)
-    !any(isnan, lapl) || throw(ArgumentError("NaN values in graph Laplacian! This is most likely due to atomic separations larger than the specified cutoff distance leading to block zeros in the adjacency matrix...try increasing the cutoff distance or inspecting your structure to ensure the file is correct."))
-    return lapl
+    st = "AtomGraph $(ag.id) with $(nv(ag.graph)) nodes, $(ne(ag.graph)) edges\n   atoms: $(ag.elements)\n   feature vector length: "
+    if isnothing(ag.featurization)
+        st = string(st, "uninitialized\n   encoded features: uninitialized")
+    else
+        st = string(
+            st,
+            "$(size(ag.features)[1])\n   encoded features: ",
+            [string(f.name, ", ") for f in g.featurization]...,
+        )[1:end-2]
+    end
+    print(io, st)
 end
-
 normalized_laplacian(ag::AtomGraph) = ag.lapl
 
 # maybe some cutesy stuff like dispatching things like length as length(elements)
@@ -122,36 +145,67 @@ normalized_laplacian(ag::AtomGraph) = ag.lapl
 function graph_colors(atno_list, seed_color=colorant"cyan4")
     atom_types = unique(atno_list)
     atom_type_inds = Dict(atom_types[i]=>i for i in 1:length(atom_types))
-    color_inds = [atom_type_inds[i] for i in atno_list]
-    colors = distinguishable_colors(length(atom_types), seed_color)
-    return colors[color_inds]
+``I - D^{-1/2} A D^{-1/2}``
+
+where ``A`` is the adjacency matrix and ``D`` is the degree matrix.
+"""
+function normalized_laplacian(g::G) where {G<:LightGraphs.AbstractGraph}
+    a = adjacency_matrix(g)
+    d = vec(sum(a, dims = 1))
+    inv_sqrt_d = diagm(0 => d .^ (-0.5f0))
+    lapl = Float32.(I - inv_sqrt_d * a * inv_sqrt_d)
+    !any(isnan, lapl) || throw(
+        ArgumentError(
+            "NaN values in graph Laplacian! This is most likely due to atomic separations larger than the specified cutoff distance leading to block zeros in the adjacency matrix...try increasing the cutoff distance or inspecting your structure to ensure the file is correct.",
+        ),
+    )
+    return lapl
 end
 
-# helper fcn for sorting because edge ordering isn't preserved when converting to SimpleGraph
-function lt_edge(e1::SimpleWeightedGraphs.SimpleWeightedEdge{<:Integer,<:Real}, e2::SimpleWeightedGraphs.SimpleWeightedEdge{<:Integer,<:Real})
-    if e1.src < e2.src
-        return true
-    elseif e1.dst < e2.dst
-        return true
-    else
-        return false
-    end
+normalized_laplacian(ag::AtomGraph) = ag.lapl
 end
 
 "Compute edge widths (proportional to weights on graph) for graph visualization."
 function graph_edgewidths(ag::AtomGraph)
     edgewidths = []
     edges_sorted = sort([e for e in edges(ag.graph)], lt=lt_edge)
+
+# now visualization stuff...
+
+"Get a list of colors to use for graph visualization."
+function graph_colors(atno_list, seed_color = colorant"cyan4")
+    atom_types = unique(atno_list)
+    atom_type_inds = Dict(atom_types[i] => i for i = 1:length(atom_types))
+    color_inds = [atom_type_inds[i] for i in atno_list]
+    colors = distinguishable_colors(length(atom_types), seed_color)
+    return colors[color_inds]
+end
+
+# helper fcn for sorting because edge ordering isn't preserved when converting to SimpleGraph
+function lt_edge(
+    e1::SimpleWeightedGraphs.SimpleWeightedEdge{<:Integer,<:Real},
+    e2::SimpleWeightedGraphs.SimpleWeightedEdge{<:Integer,<:Real},
+)
+    if e1.src < e2.src
+        return true
+    elseif e1.dst < e2.dst
+        return true
+"Compute edge widths (proportional to weights on graph) for graph visualization."
+function graph_edgewidths(ag::AtomGraph)
+    edgewidths = []
+    edges_sorted = sort([e for e in edges(ag.graph)], lt = lt_edge)
     for e in edges_sorted
         append!(edgewidths, e.weight)
     end
-    return edgewidths
-end
-
-"Visualize a given graph."
+    return edgewidths"Visualize a given graph."
 function visualize(ag::AtomGraph)
     # gplot doesn't work on weighted graphs
     sg = SimpleGraph(adjacency_matrix(ag))
-    plt = gplot(sg, nodefillc=graph_colors(ag.elements), nodelabel=ag.elements, edgelinewidth=graph_edgewidths(ag))
+    plt = gplot(
+        sg,
+        nodefillc = graph_colors(ag.elements),
+        nodelabel = ag.elements,
+        edgelinewidth = graph_edgewidths(ag),
+    )
     display(plt)
 end

.github/workflows/format_check.yml
@@ -1,0 +1,41 @@
+name: format-check
+
+on:
+  push:
+    branches:
+      - 'main'
+    tags: '*'
+  pull_request:
+
+jobs:
+  build:
+    runs-on: ${{ matrix.os }}
+    strategy:
+      matrix:
+        julia-version: [1.6.0]
+        julia-arch: [x86]
+        os: [ubuntu-latest]
+    steps:
+      - uses: julia-actions/setup-julia@latest
+        with:
+          version: ${{ matrix.julia-version }}
+
+      - uses: actions/checkout@v2
+      - name: Install JuliaFormatter and format
+        # This will use the latest version by default but you can set the version like so:
+        #
+        # julia  -e 'using Pkg; Pkg.add(PackageSpec(name="JuliaFormatter", version="0.13.0"))'
+        run: |
+          julia  -e 'using Pkg; Pkg.add(PackageSpec(name="JuliaFormatter"))'
+          julia  -e 'using JuliaFormatter; format(".", verbose=true)'
+      - name: Format check
+        run: |
+          julia -e '
+          out = Cmd(`git diff --name-only`) |> read |> String
+          if out == ""
+              exit(0)
+          else
+              @error "Some files have not been formatted !!!"
+              write(stdout, out)
+              exit(1)
+          end'

src/ChemistryFeaturization.jl
@@ -96,39 +96,16 @@ function featurize!(a::AbstractAtoms, fzn::AbstractFeaturization)
     # encode each feature in that list and assign the results to the
     # field of the same name in `a`
     for feats_list in fieldnames(fzn)
-        encoded = reduce(vcat, map((x)->x(a), feats_list))
+        encoded = reduce(vcat, map((x) -> x(a), feats_list))
         setproperty!(a, feats_list, encoded)
     end
     a.featurization = fzn
 end
 
-
 # NEXT: 
-# building features
 # building featurizations
 # featurize atomgraphs
 # update tests
-
-
-# old stuff for archival purposes for now...
-
-# export AtomFeature, atom_data_df, build_featurization, make_feature_vectors, decode_feature_vector, default_nbins
-# include("atomfeat.jl")
-
-# export AtomGraph, normalized_laplacian, add_features!, add_features_batch!, visualize_graph
-# include("atomgraph.jl")
-
-# export inverse_square, exp_decay, build_graph, build_graphs_batch, read_graphs_batch
-# include("pmg_graphs.jl")
-# using .graph_building: inverse_square, exp_decay, build_graph, build_graphs_batch, read_graphs_batch
-
-# # TODO: possibly move all the Weave stuff to another package altogether, if not need to tidy up modules/exports
-# export smiles_atom_features, smiles_bond_features
-# include("weave_fcns.jl")
-# using .weave_fcns: smiles_atom_features, smiles_bond_features, chem
-
-# export weave_featurize
-# include("featurize.jl")
-# using .featurize: weave_featurize
+# write docstrings
 
 end

src/atoms/weavemol.jl
@@ -7,7 +7,7 @@ mutable struct WeaveMol <: AbstractAtoms
     atom_feats::Vector{SomethingOrOther} # I need to look more carefully to figure this out, heh
     pair_feats::Vector{SomethingOrOther}
     featurization::WeaveFeaturization
-    id # probably makes sense to have this in addition to smiles? Maybe?
+    id::Any # probably makes sense to have this in addition to smiles? Maybe?
 end
 
-# TODO: add visualize function, either via OpenSMILES or MolecularGraph
+# TODO: add visualize function, either via OpenSMILES or MolecularGraphtion, either via OpenSMILES or MolecularGraph

src/features/atomfeature.jl
@@ -4,10 +4,10 @@ Feature of a single atom.
 May be contextual (depends on neighborhood) or elemental (defined just by the atomic identity of the node).
 =#
 # proper docstring
-struct AtomFeature<:AbstractFeature
+struct AtomFeature <: AbstractFeature
     name::String
-    encode_f
-    decode_f
+    encode_f::Any
+    decode_f::Any
     categorical::Bool
     contextual::Bool # can get from elemental lookup table (false) or not (true)?
     length::Int # length of encoded vector
@@ -15,11 +15,11 @@ end
 
 # TODO: add pretty printing
 
-# NEXT: test this constructor
 # docstring
 using ..ChemistryFeaturization.Utils.AtomFeatureUtils
-function AtomFeature(feature_name; nbins=default_nbins, logspaced=false)
-    @assert feature_name in continuous_feature_names || feature_name in categorical_feature_names "Cannot automatically build AtomFeat for $feature_name; I can't find it in a lookup table!"
+function AtomFeature(feature_name; nbins = default_nbins, logspaced = false)
+    @assert feature_name in continuous_feature_names ||
+            feature_name in categorical_feature_names "Cannot automatically build AtomFeat for $feature_name; I can't find it in a lookup table!"
     local vector_length
     categorical = feature_name in categorical_feature_names
     if categorical
@@ -27,8 +27,19 @@ function AtomFeature(feature_name; nbins=default_nbins, logspaced=false)
     else
         vector_length = nbins
     end
-    encode_f = atoms -> map(e->onehot_lookup_encoder(e, feature_name; nbins=nbins, logspaced=logspaced), atoms.elements)
-    decode_f = encoded -> onecold_lookup_decoder(encoded, feature_name; nbins=nbins, logspaced=logspaced)
+    encode_f =
+        atoms -> map(
+            e -> onehot_lookup_encoder(
+                e,
+                feature_name;
+                nbins = nbins,
+                logspaced = logspaced,
+            ),
+            atoms.elements,
+        )
+    decode_f =
+        encoded ->
+            onecold_decoder(encoded, feature_name; nbins = nbins, logspaced = logspaced)
     AtomFeature(feature_name, encode_f, decode_f, categorical, false, vector_length)
 end
 

src/features/pairfeature.jl
@@ -2,10 +2,10 @@
 Feature of a pair of atoms. Currently only used in WeaveModel, but may eventually
 have a version of AtomGraph that allows edge features...
 =#
-struct PairFeature<:AbstractFeature
-    name
-    encode_f
-    decode_f
+struct PairFeature <: AbstractFeature
+    name::String
+    encode_f::Any
+    decode_f::Any
     length::Int # maybe, maybe not (does constrain/assume vector Te)
     # probably needs some other stuff...
 end

commit be42b870ffa4781607c05c61f19699f673646127

src/atoms/atomgraph.jl: fix things broken in 513c520
513c520 introduced formatting support using JuliaFormatter. However,
this also broke a bunch of functions - normalized_laplacian(),
graph_edgewidths(), graph_colors() and long pretty printing.

Fix the regressions introduced, and restore the original functionalities
of the functions.

Fixes: 513c520 ("set up JuliaFormatter")
Signed-off-by: Anant Thazhemadam <anant.thazhemadam@gmail.com>

src/atoms/atomgraph.jl
@@ -86,6 +86,7 @@ function AtomGraph(
     AtomGraph(gr, el_list, lapl, nothing, nothing, id)
 end
 
+
 # initialize directly from adjacency matrix, defaults to 32-bit integers because unlikely to need more nodes than that...
 AtomGraph(
     adj::Array{R},
@@ -96,55 +97,42 @@ AtomGraph(
     U = UInt32,
 ) where {R<:Real} =
     AtomGraph(SimpleWeightedGraph{R}(adj), el_list, features, featurization, id)
+
 AtomGraph(adj::Array{R}, el_list::Vector{String}, id = ""; U = UInt32) where {R<:Real} =
     AtomGraph(SimpleWeightedGraph{R}(adj), el_list, id)
+
 
 # pretty printing, short version
 function Base.show(io::IO, ag::AtomGraph)
     st = "AtomGraph $(ag.id) with $(nv(ag.graph)) nodes, $(ne(ag.graph)) edges"
+    if !isnothing(ag.featurization)
+        st = string(st, ", feature vector length $(size(ag.atom_feats)[1])")
+    end
+    print(io, st)
+end
+
 # pretty printing, long version
 function Base.show(io::IO, ::MIME"text/plain", ag::AtomGraph)
     st = "AtomGraph $(ag.id) with $(nv(ag.graph)) nodes, $(ne(ag.graph)) edges\n   atoms: $(ag.elements)\n   feature vector length: "
     if isnothing(ag.featurization)
         st = string(st, "uninitialized\n   encoded features: uninitialized")
     else
-        st = string(st, "$(size(ag.features)[1])\n   encoded features: ",  [string(f.name, ", ") for f in g.featurization]...)[1:end-2]
+        st = string(
+                    st,
+                    "$(size(ag.features)[1])\n   encoded features: ",
+                    [string(f.name, ", ") for f in g.featurization]...,
+                )[1:end-2]
     end
     print(io, st)
+
 end
+
 
 """
     normalized_laplacian(graph)
 
 Compute the normalized graph Laplacian matrix of the input graph, defined as
 
-``I - D^{-1/2} A D^{-1/2}``
-
-where ``A`` is the adjacency matrix and ``D`` is the degree matrix.
-"""
-function normalized_laplacian(g::G) where G<:LightGraphs.AbstractGraph
-    st = "AtomGraph $(ag.id) with $(nv(ag.graph)) nodes, $(ne(ag.graph)) edges\n   atoms: $(ag.elements)\n   feature vector length: "
-    if isnothing(ag.featurization)
-        st = string(st, "uninitialized\n   encoded features: uninitialized")
-    else
-        st = string(
-            st,
-            "$(size(ag.features)[1])\n   encoded features: ",
-            [string(f.name, ", ") for f in g.featurization]...,
-        )[1:end-2]
-    end
-    print(io, st)
-end
-normalized_laplacian(ag::AtomGraph) = ag.lapl
-
-# maybe some cutesy stuff like dispatching things like length as length(elements)
-
-# now visualization stuff...
-
-"Get a list of colors to use for graph visualization."
-function graph_colors(atno_list, seed_color=colorant"cyan4")
-    atom_types = unique(atno_list)
-    atom_type_inds = Dict(atom_types[i]=>i for i in 1:length(atom_types))
 ``I - D^{-1/2} A D^{-1/2}``
 
 where ``A`` is the adjacency matrix and ``D`` is the degree matrix.
@@ -162,13 +150,9 @@ function normalized_laplacian(g::G) where {G<:LightGraphs.AbstractGraph}
     return lapl
 end
 
-normalized_laplacian(ag::AtomGraph) = ag.lapl
-end
 
-"Compute edge widths (proportional to weights on graph) for graph visualization."
-function graph_edgewidths(ag::AtomGraph)
-    edgewidths = []
-    edges_sorted = sort([e for e in edges(ag.graph)], lt=lt_edge)
+normalized_laplacian(ag::AtomGraph) = ag.lapl
+
 
 # now visualization stuff...
 
@@ -181,6 +165,7 @@ function graph_colors(atno_list, seed_color = colorant"cyan4")
     return colors[color_inds]
 end
 
+
 # helper fcn for sorting because edge ordering isn't preserved when converting to SimpleGraph
 function lt_edge(
     e1::SimpleWeightedGraphs.SimpleWeightedEdge{<:Integer,<:Real},
@@ -190,6 +175,12 @@ function lt_edge(
         return true
     elseif e1.dst < e2.dst
         return true
+    else
+        return false
+    end
+end
+
+
 "Compute edge widths (proportional to weights on graph) for graph visualization."
 function graph_edgewidths(ag::AtomGraph)
     edgewidths = []
@@ -197,7 +188,10 @@ function graph_edgewidths(ag::AtomGraph)
     for e in edges_sorted
         append!(edgewidths, e.weight)
     end
-    return edgewidths"Visualize a given graph."
+    return edgewidths
+end
+
+"Visualize a given graph."
 function visualize(ag::AtomGraph)
     # gplot doesn't work on weighted graphs
     sg = SimpleGraph(adjacency_matrix(ag))