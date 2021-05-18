0e81dd52453c184166bc08384d2497fb2f2ba82e
Anant Thazhemadam <anant.thazhemadam@gmail.com>
2021-05-18 07:56:42 +0530

    test: featurizations: test `encodable_elements()` with custom data
    Use `lookup_table.csv` as the custom `lookup_table` and test
    `encodable_elements()`.
    
    Signed-off-by: Anant Thazhemadam <anant.thazhemadam@gmail.com>

--- test/featurizations/GraphNodeFeaturization_tests.jl
+++ test/featurizations/GraphNodeFeaturization_tests.jl
@@ -17,7 +17,7 @@ using CSV
     @test triangle_C_1.atom_features == triangle_C_2.atom_features
 
     # test other options, make sure default values get appropriately populated...
-    df = CSV.read(abspath(@__DIR__, "..", "test_data", "lookup_table.csv"), DataFrame)
+    test_df = CSV.read(abspath(@__DIR__, "..", "test_data", "lookup_table.csv"), DataFrame)
 
 
     # encodable_elements
@@ -26,6 +26,21 @@ using CSV
         fzn4 = GraphNodeFeaturization(AtomFeature.(["7s", "Valence"]))
         @test encodable_elements(fzn3) == ["Ac", "Th", "U"]
         @test encodable_elements(fzn4) == ["Fr", "Ra", "Ac", "Th"]
+
+        # Custom lookup_table
+        feature_1 = AtomFeature("MeaningOfLife", test_df)   # zero-value case - `As` has a value = 0
+        feature_2 = AtomFeature("neg_nums", test_df)
+        feature_3 = AtomFeature("first_letter", test_df)
+        feature_4 = AtomFeature("noarsenic", test_df) # missing value case - `As` has a missing value
+
+        @test encodable_elements(GraphNodeFeaturization([feature_1, feature_2])) ==
+              ["C", "As", "Tc"]
+        @test encodable_elements(GraphNodeFeaturization([feature_1, feature_4])) ==
+              ["C", "Tc"]
+        @test encodable_elements(GraphNodeFeaturization([feature_2, feature_3])) ==
+              ["C", "As", "Tc"]
+        @test encodable_elements(GraphNodeFeaturization([feature_2, feature_4])) ==
+              ["C", "Tc"]
     end
 
 
