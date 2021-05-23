2d878de4e86f51f665a6bf7cbdd3755bf455dae6
Anant Thazhemadam <anant.thazhemadam@gmail.com>
2021-05-24 02:33:11 +0530

    atoms: WeaveMol: update `WeaveMol` to use `WeavePair`s
    In `WeaveMol`, represent features between pairs of atoms in the molecule
    in the form [`PairIndexes`, `WeavePair`].
    
    Signed-off-by: Anant Thazhemadam <anant.thazhemadam@gmail.com>

--- src/atoms/weavemol.jl
+++ src/atoms/weavemol.jl
@@ -1,12 +1,18 @@
 # some commentary
 
 # docstring...
+
+# represents a given pair of atoms by their respective indices in the adjacency matrix representation
+const PairIndexes = Tuple{Integer, Integer}
+
 mutable struct WeaveMol <: AbstractAtoms
     smiles::String
     elements::Vector{String}
-    atom_features::Vector{SomethingOrOther} # I need to look more carefully to figure this out, heh
-    pair_features::Vector{SomethingOrOther}
+    atom_features::Vector{SomethingOrOther} # Details TBD
+    # pairs encapsulates the WeavePair features for a given pair of atoms in a molecule represented using PairIndexes
+    pairs::Vector{Tuple{PairIndexes, WeavePair}}
     featurization::WeaveFeaturization
+    # graph::SimpleWeightedGraph{<:Integer,<:Real} # is something like an adjacency graph required here?
     id::Any # probably makes sense to have this in addition to smiles? Maybe?
 end
 
