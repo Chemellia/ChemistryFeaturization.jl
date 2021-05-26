e4beb740c2a840f4ed7eef3f6ada0fc79c1e8576
Anant Thazhemadam <anant.thazhemadam@gmail.com>
2021-05-26 11:53:46 +0530

    atoms: WeaveMol: change the data structure stored in the `pairs` field
    `PairIndexes` are not really necessary here, since graph iteration and
    traversal methods are well-optimised as is.
    Make the `pairs` field a vector of `PairFeature`.
    
    Since `PairIndexes` is not used anywhere else, remove `PairIndexes`
    entirely.
    
    Signed-off-by: Anant Thazhemadam <anant.thazhemadam@gmail.com>

--- src/atoms/weavemol.jl
+++ src/atoms/weavemol.jl
@@ -2,15 +2,12 @@
 
 # docstring...
 
-# represents a given pair of atoms by their respective indices in the adjacency matrix representation
-const PairIndexes = Tuple{Integer,Integer}
-
 mutable struct WeaveMol <: AbstractAtoms
     smiles::String
     elements::Vector{String}
     atom_features::Vector{SomethingOrOther} # Details TBD
     # pairs encapsulates the PairFeature features for a given pair of atoms in a molecule represented using PairIndexes
-    pairs::Vector{Tuple{PairIndexes,PairFeature}}
+    pairs::Vector{PairFeature}
     featurization::WeaveFeaturization
     # graph::SimpleWeightedGraph{<:Integer,<:Real} # is something like an adjacency graph required here?
     id::Any # probably makes sense to have this in addition to smiles? Maybe?
