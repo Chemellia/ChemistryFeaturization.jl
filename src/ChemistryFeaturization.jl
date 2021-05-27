Chemellia/ChemistryFeaturization.jl

commit 7c8a49562b4372788c2866522f1f8f38f888bcd5

new type hierarchy for features, not done fixing things so tests will fail. Still to do, at a minimum: fix tests, plus decide on terminology (encoder probably?)

src/ChemistryFeaturization.jl
@@ -3,11 +3,15 @@ module ChemistryFeaturization
 using SimpleWeightedGraphs
 using Reexport
 
-# define all the abstract types 
+# define all the abstract types
 export AbstractAtoms, AbstractFeature, AbstractFeaturization
+export AbstractPairFeature, AbstractAtomFeature, AbstractEnvironmentFeature
 abstract type AbstractAtoms end
 abstract type AbstractFeature end
 abstract type AbstractFeaturization end
+abstract type AbstractAtomFeature end
+abstract type AbstractPairFeature end
+abstract type AbstractEnvironmentFeature end
 
 include("utils/Utils.jl")
 @reexport using .Utils.AtomFeatureUtils
@@ -58,7 +62,7 @@ All subtypes should have `encode_f` and `decode_f` fields
 # link to guidance in docs about how to implement new feature types
 
 # export...
-export AtomFeature, PairFeature
+export ElementFeature, SpeciesFeature, PairFeature
 export encodable_elements, decode
 
 """
@@ -73,11 +77,13 @@ encodable_elements(fzn::AbstractFeaturization) = println("Implement me please!")
 
 # include...
 include("features/atomfeature.jl")
+include("features/speciesfeature.jl")
 include("features/pairfeature.jl")
 
 # generic encode
 function (f::AbstractFeature)(a::AbstractAtoms)
-    f.encode_f(a)
+    #f.encode_f(a)
+    println("Implement me, please!")
 end
 
 """
@@ -93,7 +99,8 @@ julia> decode(AtomFeature("Block"), [0, 1, 0, 0])
 ```
 """
 function decode(f::AbstractFeature, encoded_feature)
-    f.decode_f(encoded_feature)
+    #f.decode_f(encoded_feature)
+    println("Implement me, please!")
 end
 
 #= FEATURIZATION OBJECTS

src/features/elementfeature.jl
@@ -1,0 +1,87 @@
+using ..ChemistryFeaturization.Utils.AtomFeatureUtils
+using DataFrames
+
+# TODO: figure out what scheme would look like that is flexible to direct-value encoding (may just need a different feature type since it'll have to handle normalization, etc. too)
+"""
+    ElementFeature(feature_name, encode_f, decode_f, categorical, contextual, length, encodable_elements)
+
+Construct a feature object that encodes features associated with individual atoms that depend only upon their elemental identity (if you want to encode a feature that depends upon an atom's environment, you shold use SpeciesFeature!)
+
+## Arguments
+- `name::String`: the name of the feature
+- `categorical::Bool`: flag for whether the feature is categorical or continuous-valued
+- `length::Int`: length of encoded vector
+- `logspaced::Bool`: whether onehot-style bins should be logarithmically spaced or not
+- `lookup_table::DataFrame`: table containing values of feature for every encodable element
+"""
+struct ElementFeature <: AbstractAtomFeature
+    name::String
+    categorical::Bool
+    length::Integer
+    logspaced::Bool
+    lookup_table::DataFrame
+end
+
+# TODO: update this, encoder stuff needs to be broken out as dispatches
+# also, should trim lookup_table to just have the columns it needs before constructing object
+function ElementFeature(
+    feature_name::String,
+    lookup_table::DataFrame = atom_data_df;
+    length::Integer = default_nbins,
+    logspaced::Bool = default_log(feature_name, lookup_table),
+    categorical::Bool = default_categorical(feature_name, lookup_table),
+)
+    colnames = names(lookup_table)
+    @assert feature_name in colnames && "Symbol" in colnames "Your lookup table must have a column called :Symbol and one with the same name as your feature to be usable!"
+
+    local vector_length
+    if categorical
+        vector_length = length(unique(skipmissing(lookup_table[:, Symbol(feature_name)])))
+    else
+        vector_length = nbins
+    end
+    encode_f =
+        atoms -> reduce(
+            hcat,
+            map(
+                e -> onehot_lookup_encoder(
+                    e,
+                    feature_name,
+                    lookup_table;
+                    nbins = nbins,
+                    logspaced = logspaced,
+                    categorical = categorical,
+                ),
+                atoms.elements,
+            ),
+        )
+    decode_f =
+        encoded -> onecold_decoder(
+            encoded,
+            feature_name,
+            lookup_table;
+            nbins = nbins,
+            logspaced = logspaced,
+            categorical = categorical,
+        )
+    AtomFeature(
+        feature_name,
+        encode_f,
+        decode_f,
+        categorical,
+        false,
+        vector_length,
+        encodable_elements(feature_name, lookup_table),
+    )
+end
+
+# TODO: update this to just call thing below essentially
+#encodable_elements(f::ElementFeature) = f.encodable_elements
+
+function encodable_elements(feature_name::String, lookup_table::DataFrame = atom_data_df)
+    info = lookup_table[:, [Symbol(feature_name), :Symbol]]
+    return info[
+        findall(x -> !ismissing(x), getproperty(info, Symbol(feature_name))),
+        :Symbol,
+    ]
+end

src/features/speciesfeature.jl
@@ -1,0 +1,43 @@
+using DataFrames
+
+"""
+    SpeciesFeature(feature_name, encode_f, decode_f, categorical, contextual, length, encodable_elements)
+
+Construct a feature object that encodes features associated with individual atoms that depend upon their local environment in some way (if your feature is defined only by elemental identity, you should use ElementFeature!)
+
+## Arguments
+- `name::String`: the name of the feature
+- `encode_f::Function`: a function that takes in <:AbstractAtoms and returns encoded values of this feature for the atoms in that structure
+- `decode_f::Function`: inverse function to `encode_f`, takes in encoded feature and returns value (for categorical) or range of values (for continuous-valued) of the feature
+- `categorical::Bool`: flag for whether the feature is categorical or continuous-valued
+- `length::Int`: length of encoded vector
+- `encodable_elements::Vector{String}`: list of elements (by symbol) that can be encoded by this feature
+"""
+struct SpeciesFeature <: AbstractAtomFeature
+    name::String
+    encode_f::Function
+    decode_f::Function
+    categorical::Bool
+    length::Integer
+    encodable_elements::Vector{String}
+end
+
+
+# pretty printing, short version
+Base.show(io::IO, af::AtomFeature) = print(io, "AtomFeature $(af.name)")
+
+# pretty printing, long version
+function Base.show(io::IO, ::MIME"text/plain", af::AtomFeature)
+    st = "AtomFeature $(af.name):\n   categorical: $(af.categorical)\n   contextual: $(af.contextual)\n   encoded length: $(af.length)"
+    print(io, st)
+end
+
+# TODO: add way to get range/list of possible values for feature...
+encodable_elements(f::SpeciesFeature) = f.encodable_elements
+
+function (f::SpeciesFeature)(a::AbstractAtoms)
+    @assert all([el in f.encodable_elements for el in a.elements]) "Feature $(f.name) cannot encode some element(s) in this structure!"
+    f.encode_f(a)
+end
+
+# TODO: some Weave stuff needed here?

src/featurizations/graphnodefeaturization.jl
@@ -20,12 +20,12 @@ The "convenience constructor" that builds the AtomFeature objects for you only s
 - `categorical::Union{Vector{Bool},Bool}`: Whether each feature is categorical or continous-valued.
 """
 struct GraphNodeFeaturization <: AbstractFeaturization
-    atom_features::Vector{AtomFeature}
+    atom_features::Vector{<:AbstractAtomFeature}
 end
 
 function GraphNodeFeaturization(
-    feature_names::Vector{String};
-    lookup_table::Union{DataFrame,Nothing} = nothing,
+    feature_names::Vector{String},
+    lookup_table::Union{DataFrame,Nothing} = nothing;
     nbins::Union{Vector{Integer},Integer,Nothing} = nothing,
     logspaced::Union{Vector{Bool},Bool,Nothing} = nothing,
     categorical::Union{Vector{Bool},Bool,Nothing} = nothing,
@@ -116,6 +116,15 @@ function decode(fzn::GraphNodeFeaturization, encoded::Matrix{<:Real})
             #println("    $(f.name): $(decode(f, chunk))")
             decoded[i][f.name] = decode(f, chunk)
         end
+    end
+    return decoded
+end
+
+function decode(ag::AtomGraph)
+    @assert !(all(isnothing.(ag.featurization, ag.atom_features))
+    decoded = decode(ag.fzn, ag.atom_features)
+    for (k,v) in decoded
+        v["Symbol"] = ag.elements[k]
     end
     return decoded
 end

src/utils/elementfeature_utils.jl
@@ -1,7 +1,7 @@
 #=
 This module houses the built-in feature values for a variety of non-contextual atom features and also some convenience functions for constructing them easily.
 =#
-module AtomFeatureUtils
+module ElementFeatureUtils
 
 using DataFrames
 using CSV
@@ -13,7 +13,7 @@ export default_nbins, oom_threshold_log
 export atom_data_df, avail_feature_names
 export categorical_feature_names, categorical_feature_vals, continuous_feature_names
 export default_log, fea_minmax, default_categorical
-export get_bins, build_onehot_vec
+export get_bins, build_onehot_vec, get_param_vec
 export onehot_lookup_encoder, onecold_decoder, encodable_elements
 
 # default number of bins for continuous features, if unspecified

commit 0408ada7bd4fc5be7cc71ea894ddf0040a6718ef

fix tests, and other regressions introduced in 7c8a495
* Rename all previous mentions of `AtomFeatureDescriptor` appropriately
(to `ElementFeatureDescriptor` or `SpeciesFeatureDescriptor`)

* Update instances where `.Utils.AtomFeatureUtils` module is used to
`.Utils.ElementFeatureUtils`.

* `encode_f` and `decode_f` were removed as fields of
`ElementFeatureDescriptor`. In their place, define the actual
encode/decode functions directly in the interface which used to be
responsible for calling them anyways.
This is reasonable for `ElementFeatureDescriptor`, since encode/decode
functions for it will always be the same, and depend only on the
parameters that are stored.

* Add an `nbins` field to `ElementFeatureDescriptor`. Since `encode_f`,
and `decode_f` are no longer fields of `ElementFeatureDescriptor`, there
is no real way to encode features into the bins as required by the user.
Having `nbins` as a field allows us to do this, and also makes sense
intuitively as well (since `ElementFeatureDescriptor` essentially
describes the encoding/decoding procedure and binning is a part of that).

* Remove the custom "silly encode/decode" tests for
`ElementFeatureDescriptor`. These could however be later re-introduced
in some other manner for testing `SpeciesFeatureDescriptor`.

* Fix syntax error caused by a missing closing parentheses in
`decode(::AtomGraph)`

Fixes: 7c8a495 ("new type hierarchy for features, not done fixing things
so tests will fail. Still to do, at a minimum: fix tests, plus decide on
terminology (encoder probably?)")
Signed-off-by: Anant Thazhemadam <anant.thazhemadam@gmail.com>

src/ChemistryFeaturization.jl
@@ -14,7 +14,7 @@ abstract type AbstractPairFeatureDescriptor end
 abstract type AbstractEnvironmentFeatureDescriptor end
 
 include("utils/Utils.jl")
-@reexport using .Utils.AtomFeatureUtils
+@reexport using .Utils.ElementFeatureUtils
 @reexport using .Utils.GraphBuilding
 
 #= ATOMS OBJECTS
@@ -76,13 +76,12 @@ encodable_elements(f::AbstractFeatureDescriptor) = println("Implement me please!
 encodable_elements(fzn::AbstractFeaturization) = println("Implement me please!")
 
 # include...
-include("features/atomfeature.jl")
+include("features/elementfeature.jl")
 include("features/speciesfeature.jl")
 include("features/pairfeature.jl")
 
 # generic encode
 function (f::AbstractFeatureDescriptor)(a::AbstractAtoms)
-    #f.encode_f(a)
     println("Implement me, please!")
 end
 

src/features/elementfeature.jl
@@ -1,4 +1,4 @@
-using ..ChemistryFeaturization.Utils.AtomFeatureUtils
+using ..ChemistryFeaturization.Utils.ElementFeatureUtils
 using DataFrames
 
 # TODO: figure out what scheme would look like that is flexible to direct-value encoding (may just need a different feature type since it'll have to handle normalization, etc. too)
@@ -16,9 +16,10 @@ Construct a feature object that encodes features associated with individual atom
 """
 struct ElementFeatureDescriptor <: AbstractAtomFeatureDescriptor
     name::String
-    categorical::Bool
     length::Integer
+    nbins::Integer
     logspaced::Bool
+    categorical::Bool
     lookup_table::DataFrame
 end
 
@@ -27,9 +28,9 @@ end
 function ElementFeatureDescriptor(
     feature_name::String,
     lookup_table::DataFrame = atom_data_df;
-    length::Integer = default_nbins,
+    nbins::Integer = default_nbins,
     logspaced::Bool = default_log(feature_name, lookup_table),
-    categorical::Bool = default_categorical(feature_name, lookup_table),
+    categorical::Bool = default_categorical(feature_name, lookup_table)
 )
     colnames = names(lookup_table)
     @assert feature_name in colnames && "Symbol" in colnames "Your lookup table must have a column called :Symbol and one with the same name as your feature to be usable!"
@@ -40,43 +41,29 @@ function ElementFeatureDescriptor(
     else
         vector_length = nbins
     end
-    encode_f =
-        atoms -> reduce(
-            hcat,
-            map(
-                e -> onehot_lookup_encoder(
-                    e,
-                    feature_name,
-                    lookup_table;
-                    nbins = nbins,
-                    logspaced = logspaced,
-                    categorical = categorical,
-                ),
-                atoms.elements,
-            ),
-        )
-    decode_f =
-        encoded -> onecold_decoder(
-            encoded,
-            feature_name,
-            lookup_table;
-            nbins = nbins,
-            logspaced = logspaced,
-            categorical = categorical,
-        )
-    AtomFeatureDescriptor(
+
+    ElementFeatureDescriptor(
         feature_name,
-        encode_f,
-        decode_f,
-        categorical,
-        false,
         vector_length,
-        encodable_elements(feature_name, lookup_table),
+        nbins,
+        logspaced,
+        categorical,
+        lookup_table
     )
 end
 
-# TODO: update this to just call thing below essentially
-#encodable_elements(f::ElementFeatureDescriptor) = f.encodable_elements
+# pretty printing, short version
+Base.show(io::IO, af::ElementFeatureDescriptor) = print(io, "ElementFeature $(af.name)")
+
+# pretty printing, long version
+function Base.show(io::IO, ::MIME"text/plain", af::ElementFeatureDescriptor)
+    st = "ElementFeature $(af.name):\n   categorical: $(af.categorical)\n   contextual: $(af.contextual)\n   encoded length: $(af.length)"
+    print(io, st)
+end
+
+# TODO: add way to get range/list of possible values for feature...
+
+encodable_elements(f::ElementFeatureDescriptor) = encodable_elements(f.name, f.lookup_table)
 
 function encodable_elements(feature_name::String, lookup_table::DataFrame = atom_data_df)
     info = lookup_table[:, [Symbol(feature_name), :Symbol]]
@@ -85,3 +72,40 @@ function encodable_elements(feature_name::String, lookup_table::DataFrame = atom
         :Symbol,
     ]
 end
+
+function (f::ElementFeatureDescriptor)(a::AbstractAtoms)
+    @assert all([el in encodable_elements(f) for el in a.elements]) "Feature $(f.name) cannot encode some element(s) in this structure!"
+    reduce(
+        hcat,
+            map(
+                e -> onehot_lookup_encoder(
+                    e,
+                    f.name,
+                    f.lookup_table;
+                    nbins = f.nbins,
+                    logspaced = f.logspaced,
+                    categorical = f.categorical,
+            ),
+            a.elements,
+        )
+    )
+end
+
+#=
+    decode_f =
+        encoded_feature -> onecold_decoder(
+            encoded_feature,
+            feature_name,
+            lookup_table;
+            nbins = nbins,
+            logspaced = logspaced,
+            categorical = categorical,
+        )
+=#
+
+function decode(f::ElementFeatureDescriptor, encoded_feature)
+    onecold_decoder(encoded_feature, f.name, f.lookup_table;
+                    nbins = f.nbins, logspaced = f.logspaced, categorical = f.categorical)
+end
+
+

src/features/speciesfeature.jl
@@ -24,10 +24,10 @@ end
 
 
 # pretty printing, short version
-Base.show(io::IO, af::AtomFeatureDescriptor) = print(io, "AtomFeature $(af.name)")
+Base.show(io::IO, af::SpeciesFeatureDescriptor) = print(io, "AtomFeature $(af.name)")
 
 # pretty printing, long version
-function Base.show(io::IO, ::MIME"text/plain", af::AtomFeatureDescriptor)
+function Base.show(io::IO, ::MIME"text/plain", af::SpeciesFeatureDescriptor)
     st = "AtomFeature $(af.name):\n   categorical: $(af.categorical)\n   contextual: $(af.contextual)\n   encoded length: $(af.length)"
     print(io, st)
 end

src/featurizations/graphnodefeaturization.jl
@@ -59,7 +59,7 @@ function GraphNodeFeaturization(
     end
 
     afs = map(zip(feature_names, nbins_here, logspaced_here, categorical_here)) do args
-        AtomFeatureDescriptor(
+        ElementFeatureDescriptor(
             args[1],
             lookup_table_here,
             nbins = args[2],
@@ -73,7 +73,7 @@ end
 # TODO: function to compute total vector length?
 
 encodable_elements(fzn::GraphNodeFeaturization) =
-    intersect([f.encodable_elements for f in fzn.atom_features]...)
+    intersect([encodable_elements(f) for f in fzn.atom_features]...)
 
 """
     chunk_vec(vec, nbins)
@@ -121,7 +121,7 @@ function decode(fzn::GraphNodeFeaturization, encoded::Matrix{<:Real})
 end
 
 function decode(ag::AtomGraph)
-    @assert !(all(isnothing.(ag.featurization, ag.atom_features))
+    @assert !(all(isnothing.(ag.featurization, ag.atom_features)))
     decoded = decode(ag.fzn, ag.atom_features)
     for (k,v) in decoded
         v["Symbol"] = ag.elements[k]

src/utils/Utils.jl
@@ -1,8 +1,8 @@
 module Utils
 
 using Reexport
-include("atomfeature_utils.jl")
-@reexport using .AtomFeatureUtils
+include("elementfeature_utils.jl")
+@reexport using .ElementFeatureUtils
 
 include("graph_building.jl")
 @reexport using .GraphBuilding

src/utils/elementfeature_utils.jl
@@ -182,7 +182,7 @@ function build_onehot_vec(val, bins, categorical)
     return onehot_vec
 end
 
-"Function for encoding onehot-style vectors based on values in a lookup table. Intended to be used as an `encode_f` for `AtomFeatureDescriptor` objects. See source code of convenience constructor for `AtomFeatureDescriptor` for more details."
+"Function for encoding onehot-style vectors based on values in a lookup table. Intended to be used as an `encode_f` for `ElementFeatureDescriptor` objects. See source code of convenience constructor for `ElementFeatureDescriptor` for more details."
 function onehot_lookup_encoder(
     el::String,
     feature_name::String,
@@ -211,7 +211,7 @@ function onehot_lookup_encoder(
     build_onehot_vec(val, bins, categorical)
 end
 
-"Function for decoding onehot-style vectors based on values in a lookup table. Intended to be used as an `decode_f` for `AtomFeatureDescriptor` objects. See source code of convenience constructor for `AtomFeatureDescriptor` for more details."
+"Function for decoding onehot-style vectors based on values in a lookup table. Intended to be used as an `decode_f` for `ElementFeatureDescriptor` objects. See source code of convenience constructor for `ElementFeatureDescriptor` for more details."
 function onecold_decoder(
     encoded,
     feature_name::String,

test/atoms/AtomGraph_tests.jl
@@ -3,7 +3,7 @@ using LightGraphs
 using SimpleWeightedGraphs
 using Serialization
 
-# NB: featurizing graphs is tested in AtomFeatureDescriptor and GraphNodeFeaturization tests
+# NB: featurizing graphs is tested in ElementFeatureDescriptor and GraphNodeFeaturization tests
 
 @testset "AtomGraph" begin
 

test/features/ElementFeature_tests.jl
@@ -3,39 +3,45 @@ using DataFrames
 using CSV
 const cf = ChemistryFeaturization
 
-@testset "AtomFeatureDescriptor" begin
-    # test for errors...
-    @test_throws AssertionError AtomFeatureDescriptor("heffalump")
+@testset "ElementFeatureDescriptor" begin
+    # test for errors
+    @test_throws AssertionError ElementFeatureDescriptor("heffalump")
 
     # construct a few from built-in features...
     local fnames = ["X", "Block", "Atomic mass"]
     local lengths = [default_nbins, 4, default_nbins]
     local cats = [false, true, false]
     local conts = [false, false, false]
+
     for i = 1:3
-        f = AtomFeatureDescriptor(fnames[i])
+        f = ElementFeatureDescriptor(fnames[i])
         @test f.length == lengths[i]
         @test f.categorical == cats[i]
-        @test f.contextual == conts[i]
     end
 
-    # encode/decode some stuff...
+    # # encode/decode some stuff...
     He_mol = AtomGraph(Float32.([1 0; 0 1]), ["He", "He"])
-    X, block, amass = AtomFeatureDescriptor.(fnames)
+    X, block, amass = ElementFeatureDescriptor.(fnames)
     @test_throws AssertionError X(He_mol)
+
     @test block(He_mol) == Float64.([0 0; 1 1; 0 0; 0 0])
+
     @test decode(block, block(He_mol)) == ["p", "p"]
+
     @test amass(He_mol)[3, :] == ones(2)
+
     true_He_amass = atom_data_df[2, Symbol("Atomic mass")]
     He_amass_min, He_amass_max = decode(amass, amass(He_mol)[:, 1])
     @test He_amass_min < true_He_amass < He_amass_max
 
     # now let's try some options (and make sure they're ignored when appropriate...)...
-    X = AtomFeatureDescriptor("X", nbins = 8, logspaced = true)
-    block = AtomFeatureDescriptor("Block", nbins = 5)
+    X = ElementFeatureDescriptor("X", nbins = 8, logspaced = true)
     triangle_C = AtomGraph(Float32.([0 1 1; 1 0 1; 1 1 0]), ["C", "C", "C"])
     @test X(triangle_C)[6, :] == ones(3)
+    
+    block = ElementFeatureDescriptor("Block", nbins = 5)
     @test block(triangle_C)[2, :] == ones(3)
+
     true_X = atom_data_df.X[6]
     decoded_X_range = decode(X, X(triangle_C)[:, 1])
     @test decoded_X_range[1] < true_X < decoded_X_range[2]
@@ -43,17 +49,8 @@ const cf = ChemistryFeaturization
 
     # and make a custom lookup table...
     df = CSV.read(abspath(@__DIR__, "..", "test_data", "lookup_table.csv"), DataFrame)
-    meaning = AtomFeatureDescriptor("MeaningOfLife", df)
+    meaning = ElementFeatureDescriptor("MeaningOfLife", df)
     @test meaning(triangle_C)[10, :] == ones(3)
     @test encodable_elements(meaning) == ["C", "As", "Tc"]
     @test encodable_elements("MeaningOfLife", df) == ["C", "As", "Tc"]
-
-    # make a totally custom one, with a silly encode/decode
-    element_encoder(element::String) = length(element)
-    atoms_encoder = atoms -> reduce(hcat, map(e -> element_encoder(e), atoms.elements))
-    decoder(encoded::Int) = string(['a' for i = 1:encoded]...)
-    sillyfeature =
-        AtomFeatureDescriptor("silly", atoms_encoder, decoder, false, false, 1, ["C", "Ne"])
-    @test sillyfeature(triangle_C) == ones(1, 3)
-    @test encodable_elements(sillyfeature) == ["C", "Ne"]
 end

test/featurizations/GraphNodeFeaturization_tests.jl
@@ -6,7 +6,7 @@ using CSV
 
     # make sure both constructors give the same results
     local fnames = ["X", "Block", "Atomic mass"]
-    features = AtomFeatureDescriptor.(fnames)
+    features = ElementFeatureDescriptor.(fnames)
     fzn1 = GraphNodeFeaturization(features)
     fzn2 = GraphNodeFeaturization(fnames)
 
@@ -18,16 +18,16 @@ using CSV
 
     # encodable_elements
     @testset "Encodable Elements" begin
-        fzn3 = GraphNodeFeaturization(AtomFeatureDescriptor.(["Boiling point", "6d"]))
-        fzn4 = GraphNodeFeaturization(AtomFeatureDescriptor.(["7s", "Valence"]))
+        fzn3 = GraphNodeFeaturization(ElementFeatureDescriptor.(["Boiling point", "6d"]))
+        fzn4 = GraphNodeFeaturization(ElementFeatureDescriptor.(["7s", "Valence"]))
         @test encodable_elements(fzn3) == ["Ac", "Th", "U"]
         @test encodable_elements(fzn4) == ["Fr", "Ra", "Ac", "Th"]
 
         # Custom lookup_table
-        feature_1 = AtomFeatureDescriptor("MeaningOfLife", test_df)   # zero-value case - `As` has a value = 0
-        feature_2 = AtomFeatureDescriptor("neg_nums", test_df)
-        feature_3 = AtomFeatureDescriptor("first_letter", test_df)
-        feature_4 = AtomFeatureDescriptor("noarsenic", test_df) # missing value case - `As` has a missing value
+        feature_1 = ElementFeatureDescriptor("MeaningOfLife", test_df)   # zero-value case - `As` has a value = 0
+        feature_2 = ElementFeatureDescriptor("neg_nums", test_df)
+        feature_3 = ElementFeatureDescriptor("first_letter", test_df)
+        feature_4 = ElementFeatureDescriptor("noarsenic", test_df) # missing value case - `As` has a missing value
 
         @test encodable_elements(GraphNodeFeaturization([feature_1, feature_2])) ==
               ["C", "As", "Tc"]

test/runtests.jl
@@ -4,10 +4,10 @@ using Test
 const testdir = dirname(@__FILE__)
 
 tests = [
-	"utils/AtomFeatureUtils_tests",
+	"utils/ElementFeatureUtils_tests",
 	"utils/GraphBuilding_tests",
 	"atoms/AtomGraph_tests",
-	"features/AtomFeature_tests",
+	"features/ElementFeature_tests",
 	"featurizations/GraphNodeFeaturization_tests",
 	# TODO: add Weave stuff
 ]

test/utils/ElementFeatureUtils_tests.jl
@@ -2,9 +2,9 @@ using Test
 using DataFrames
 using CSV
 const cf = ChemistryFeaturization
-const afu = ChemistryFeaturization.Utils.AtomFeatureUtils
+const afu = ChemistryFeaturization.Utils.ElementFeatureUtils
 
-@testset "AtomFeatureUtils" begin
+@testset "ElementFeatureUtils" begin
     df = CSV.read(abspath(@__DIR__, "..", "test_data", "lookup_table.csv"), DataFrame)
 
     # fea_minmax