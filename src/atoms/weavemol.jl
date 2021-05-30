b456702dcee077fc81b7b611d416bbc42d027fb6
Anant Thazhemadam <anant.thazhemadam@gmail.com>
2021-05-30 21:41:20 +0530

    atoms: WeaveMol: add fields for adjacency graph and encoded features
    Signed-off-by: Anant Thazhemadam <anant.thazhemadam@gmail.com>

--- src/atoms/weavemol.jl
+++ src/atoms/weavemol.jl
@@ -5,11 +5,10 @@
 mutable struct WeaveMol <: AbstractAtoms
     smiles::String
     elements::Vector{String}
-    atom_features::Vector{SomethingOrOther} # Details TBD
-    # pairs encapsulates the PairFeatureDescriptor features for a given pair of atoms in a molecule represented using PairIndexes
-    pairs::Vector{PairFeatureDescriptor}
+    graph::SimpleWeightedGraph{<:Integer,<:Real}
+    encoded_atom_features::Union{Matrix{<:Real},Nothing}
+    encoded_pair_features::Union{Matrix{Union{Vector{<:Real}, Nothing}},Nothing}
     featurization::WeaveFeaturization
-    # graph::SimpleWeightedGraph{<:Integer,<:Real} # is something like an adjacency graph required here?
     id::Any # probably makes sense to have this in addition to smiles? Maybe?
 end
 
