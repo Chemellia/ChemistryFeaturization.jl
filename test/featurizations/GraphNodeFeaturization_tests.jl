561720b2aed870210bea0584744adf0ea6469046
Rachel Kurchin <rkurchin@cmu.edu>
2021-06-03 14:17:13 -0400

    updates to GNF tests that hadn't been committed

--- test/featurizations/GraphNodeFeaturization_tests.jl
+++ test/featurizations/GraphNodeFeaturization_tests.jl
@@ -4,17 +4,32 @@ using CSV
 
 @testset "GraphNodeFeaturization" begin
 
-    # make sure both constructors give the same results
-    local fnames = ["X", "Block", "Atomic mass"]
-    features = AtomFeature.(fnames)
-    fzn1 = GraphNodeFeaturization(features)
-    fzn2 = GraphNodeFeaturization(fnames)
-
-    triangle_C_1 = AtomGraph(Float32.([0 1 1; 1 0 1; 1 1 0]), ["C", "C", "C"])
-    triangle_C_2 = AtomGraph(Float32.([0 1 1; 1 0 1; 1 1 0]), ["C", "C", "C"])
-
-    # test other options, make sure default values get appropriately populated...
+    # custom lookup table...
     test_df = CSV.read(abspath(@__DIR__, "..", "test_data", "lookup_table.csv"), DataFrame)
+
+    @testset "Construct, encode, decode"
+        # make sure both constructors give the same results
+        local fnames = ["X", "Block", "Atomic mass"]
+        features = AtomFeature.(fnames)
+        fzn1 = GraphNodeFeaturization(features)
+        fzn2 = GraphNodeFeaturization(fnames)
+
+        triangle_C_1 = AtomGraph(Float32.([0 1 1; 1 0 1; 1 1 0]), ["C", "C", "C"])
+        triangle_C_2 = AtomGraph(Float32.([0 1 1; 1 0 1; 1 1 0]), ["C", "C", "C"])
+        # (this one also tests broadcasting of featurize!)
+        featurize!.([triangle_C_1, triangle_C_2], [fzn1, fzn2])
+        @test triangle_C_1.atom_features == triangle_C_2.atom_features
+
+        # test that other options work properly
+        fzn3 = GraphNodeFeaturization(fnames, nbins=2)
+        F2 = AtomGraph(Float32.([0 1; 1 0]), ["F", "F"])
+        featurize!(F2, fzn3)
+        decoded_matrix = decode(fzn3, F2.atom_features)
+        decoded_ag = decode(fzn3, F2)
+        @test all(map(d->d[1]["Block"]==d[2]["Block"]=="p", [decoded_matrix, decoded_ag])
+
+
+    end
 
     # encodable_elements
     @testset "Encodable Elements" begin
@@ -39,18 +54,10 @@ using CSV
               ["C", "Tc"]
     end
 
-
     # chunk_vec helper fcn
     @testset "chunk_vec" begin
         vec = [1, 1, 0, 1, 0, 1, 0]
         @test_throws AssertionError chunk_vec(vec, [3, 3])
         @test chunk_vec(vec, [4, 1, 2]) == [[1, 1, 0, 1], [0], [1, 0]]
     end
-
-    # featurize!
-    featurize!.([triangle_C_1, triangle_C_2], [fzn1, fzn2])
-    @test triangle_C_1.atom_features == triangle_C_2.atom_features
-
-    # decode
-
 end
