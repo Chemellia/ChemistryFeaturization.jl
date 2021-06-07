b8fae4b0bb7a0a2622b1d61df6a0c49d0d5c753f
Anant Thazhemadam <anant.thazhemadam@gmail.com>
2021-06-07 20:07:51 +0530

    docs: make.jl: define more options in `makedocs`
    Define `format` and `linkcheck` options in the `makedocs` function call.
    
    Signed-off-by: Anant Thazhemadam <anant.thazhemadam@gmail.com>

--- docs/make.jl
+++ docs/make.jl
@@ -10,7 +10,14 @@ makedocs(
     "AtomGraph" => "types/atomgraph.md",
     "Building Atomic Graphs" => "build_ag.md",
         "Changelog" => "changelog.md",    
-    ]
+    ],
+	format = Documenter.HTML(
+        # Use clean URLs, unless built as a "local" build
+        prettyurls = !("local" in ARGS),
+        canonical = "https://chemellia.github.io/ChemistryFeaturization.jl/stable/",
+        edit_link = "main",
+    ),
+    linkcheck = "linkcheck" in ARGS
 )
 deploydocs(
     repo = "github.com/Chemellia/ChemistryFeaturization.jl.git",
