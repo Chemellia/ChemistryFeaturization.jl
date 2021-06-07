ca4cf9ef741ca1fe7aafd9ae37b78cf2cb8197d3
Rachel Kurchin <rkurchin@cmu.edu>
2021-06-07 16:48:58 -0400

    formatting

--- docs/make.jl
+++ docs/make.jl
@@ -4,27 +4,27 @@ makedocs(
     sitename = "ChemistryFeaturization.jl",
     modules = [ChemistryFeaturization],
     pages = Any[
-    "Home" => "index.md",
-    "Terminology" => "terminology.md",
-    "Types" => Any[
-        "Atoms Objects" => "types/atoms.md",
-        "Feature Descriptors" => "types/feature_descriptors.md",
-        "Featurizations" => "types/featurizations.md"
+        "Home"=>"index.md",
+        "Terminology"=>"terminology.md",
+        "Types"=>Any[
+            "Atoms Objects"=>"types/atoms.md",
+            "Feature Descriptors"=>"types/feature_descriptors.md",
+            "Featurizations"=>"types/featurizations.md",
+        ],
+        "Contributing"=>"contributing.md",
+        "Changelog"=>"changelog.md",
     ],
-    "Contributing" => "contributing.md",
-    "Changelog" => "changelog.md",    
-    ],
-	format = Documenter.HTML(
+    format = Documenter.HTML(
         # Use clean URLs, unless built as a "local" build
         prettyurls = !("local" in ARGS),
         canonical = "https://chemellia.github.io/ChemistryFeaturization.jl/stable/",
         edit_link = "main",
     ),
-    linkcheck = "linkcheck" in ARGS
+    linkcheck = "linkcheck" in ARGS,
 )
 deploydocs(
     repo = "github.com/Chemellia/ChemistryFeaturization.jl.git",
     target = "build",
     devbranch = "main",
-    branch = "gh-pages"
+    branch = "gh-pages",
 )
