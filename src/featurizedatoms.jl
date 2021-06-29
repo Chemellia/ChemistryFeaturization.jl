fa8595b9710b367832992e52ba24da2929cfe259
Anant Thazhemadam <anant.thazhemadam@gmail.com>
2021-06-30 04:43:12 +0530

    featurizedatoms: make `FeaturizedAtoms` a parametric type
    * Make `FeaturizedAtoms` a parametric type that takes two parameters -
    `A`, `F` where `A` <: `AbstractAtoms` and `F` <: `AbstractFeaturization`
    
    * Create an outer constructor for the parametric type that allows
    for creation a `FeaturizedAtoms` objects even if parameters aren't
    explicitly mentioned (as long as the arguments passed are valid, and are
    of compatible types)
    
    Signed-off-by: Anant Thazhemadam <anant.thazhemadam@gmail.com>

--- src/featurizedatoms.jl
+++ src/featurizedatoms.jl
@@ -8,15 +8,22 @@ Container object for an `Atoms` object, a `featurization`, and the resulting
 
 Note: `encoded_features` will NOT change for a given atoms-featurization pair.
 """
-struct FeaturizedAtoms
-    atoms::AbstractAtoms
-    featurization::AbstractFeaturization
+struct FeaturizedAtoms{A<:AbstractAtoms,F<:AbstractFeaturization}
+    atoms::A
+    featurization::F
     encoded_features::Any
-
-    # inner constructor - generates `encoded_features`
-    FeaturizedAtoms(atoms::AbstractAtoms, featurization::AbstractFeaturization) =
+    FeaturizedAtoms{A,F}(
+        atoms,
+        featurization,
+    ) where {A<:AbstractAtoms,F<:AbstractFeaturization} =
         new(atoms, featurization, featurize(atoms, featurization))
 end
+
+FeaturizedAtoms(
+    atoms::A,
+    featurization::F,
+) where {A<:AbstractAtoms,F<:AbstractFeaturization} =
+    FeaturizedAtoms{A,F}(atoms, featurization)
 
 function decode(featurized_atoms::FeaturizedAtoms)
     decoded = decode(featurized_atoms.featurization, featurized_atoms.encoded_features)
