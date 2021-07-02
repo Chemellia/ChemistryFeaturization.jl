Chemellia/ChemistryFeaturization.jl

commit 7257df3b54ad5d76ad6f7a1152d5f5953856064c

atomgraph: initialize `id` correctly while generating graph from a file
* If an `id` is not explicitly specified, instead of setting `id` to "",
set it equal to `input_file_path`

* Fix syntax for keyword arguments in constructor that generates an
`AtomGraph` from a file.

Signed-off-by: Anant Thazhemadam <anant.thazhemadam@gmail.com>

src/atoms/atomgraph.jl
@@ -79,7 +79,7 @@ Construct an AtomGraph object from a structure file.
 """
 function AtomGraph(
     input_file_path::String,
-    id::String = "",
+    id::String = input_file_path,
     output_file_path::Union{String,Nothing} = nothing,
     overwrite_file::Bool = false,
     use_voronoi::Bool = false,
@@ -98,9 +98,7 @@ function AtomGraph(
 
     if splitext(input_file_path)[end] == ".jls" # deserialize
         ag = deserialize(input_file_path)
-        if isempty(id)   # if an id is specified, use that instead of whatever id is got from deserializing
-            ag.id = id
-        end
+        ag.id = id
 
     else # try actually building the graph
         try

commit 1a643cb272afae751c13f50e243a0490c58b2a39

atomgraph: fix constructor- bring back keyword args (building from file)
* While the constructor that builds `AtomGraph` from files was updated in
7257df3, the line that had the ';' which specified the beginning of the
keyword arguments was deleted, but the ';' wasn't specified.

Specify the ';' after the `id` argument, and reinstate support for
keyword arguments.

* Refactor a little inside the try block (while actually building the
graph) in the same constructor, and update its docstring too.

Fixes: 7257df3 ("atomgraph: initialize `id` correctly while generating graph from a file")
Signed-off-by: Anant Thazhemadam <anant.thazhemadam@gmail.com>

src/atoms/atomgraph.jl
@@ -52,7 +52,7 @@ AtomGraph(adj::Array{R}, elements::Vector{String}, id = "") where {R<:Real} =
     AtomGraph(SimpleWeightedGraph(adj), elements, id)
 
 """
-    AtomGraph(input_file_path; id="", output_file_path=nothing, featurization=nothing, overwrite_file=false, use_voronoi=false, cutoff_radius=8.0, max_num_nbr=12, dist_decay_func=inverse_square, normalize_weights=true)
+    AtomGraph(input_file_path, id = splitext(input_file_path)[begin]; output_file_path = nothing, featurization = nothing, overwrite_file = false, use_voronoi = false, cutoff_radius = 8.0, max_num_nbr = 12, dist_decay_func = inverse_square, normalize_weights = true)
 
 Construct an AtomGraph object from a structure file.
 
@@ -60,22 +60,21 @@ Construct an AtomGraph object from a structure file.
 - `input_file_path::String`: path to file containing structure (must be readable by ASE.io.read)
 
 # Optional Arguments
-- `id::String=""`: ID associated with structure (e.g. identifier from online database)
-- `output_file_path=nothing`: If provided, structure will be serialized to file at this location
-- `featurization`: If provided, features will be encoded using it
-- `overwrite_file::Bool=false`: whether to overwrite an existing file at `output_file_path`
-- `use_voronoi::Bool=false`: Whether to build neighbor lists using Voronoi decompositions
-- `cutoff_radius::Real=8.0`: If not using Voronoi neighbor lists, longest allowable distance to a neighbor, in Angstroms
-- `max_num_nbr::Integer=12`: If not using Voronoi neighbor lists, largest allowable number of neighbors
-- `dist_decay_func=inverse_square`: Function by which to assign edge weights according to distance between neighbors
-- `normalize_weights::Bool=true`: Whether to normalize weights such that the largest is 1.0
+- `id::String`: ID associated with structure (e.g. identifier from online database). Defaults to name of input file if undefined.
+- `output_file_path = nothing`: If provided, structure will be serialized to file at this location
+- `overwrite_file::Bool = false`: whether to overwrite an existing file at `output_file_path`
+- `use_voronoi::Bool = false`: Whether to build neighbor lists using Voronoi decompositions
+- `cutoff_radius::Real = 8.0`: If not using Voronoi neighbor lists, longest allowable distance to a neighbor, in Angstroms
+- `max_num_nbr::Integer = 12`: If not using Voronoi neighbor lists, largest allowable number of neighbors
+- `dist_decay_func = inverse_square`: Function by which to assign edge weights according to distance between neighbors
+- `normalize_weights::Bool = true`: Whether to normalize weights such that the largest is 1.0
 
 # Note
 `max_num_nbr` is a "soft" limit – if multiple neighbors are at the same distance, the full neighbor list may be longer.
 """
 function AtomGraph(
     input_file_path::String,
-    id::String = splitext(input_file_path)[begin],
+    id::String = splitext(input_file_path)[begin];
     output_file_path::Union{String,Nothing} = nothing,
     overwrite_file::Bool = false,
     use_voronoi::Bool = false,
@@ -98,17 +97,15 @@ function AtomGraph(
 
     else # try actually building the graph
         try
-            ag = AtomGraph(
-                build_graph(
-                    input_file_path,
-                    use_voronoi = use_voronoi,
-                    cutoff_radius = cutoff_radius,
-                    max_num_nbr = max_num_nbr,
-                    dist_decay_func = dist_decay_func,
-                    normalize_weights = normalize_weights,
-                )...,
-                id,
+            adj_mat, elements = build_graph(
+                input_file_path,
+                use_voronoi = use_voronoi,
+                cutoff_radius = cutoff_radius,
+                max_num_nbr = max_num_nbr,
+                dist_decay_func = dist_decay_func,
+                normalize_weights = normalize_weights,
             )
+            ag = AtomGraph(adj_mat, elements, id)
         catch
             @warn "Unable to build graph for $input_file_path"
             return missing