69bd611e9537f0ce915a905090cb2cab5d468c7e
Anant Thazhemadam <anant.thazhemadam@gmail.com>
2021-07-08 05:53:08 +0530

    codec: create a simple codec - `SimpleCodec`
    Define `SimpleCodec` - a very simple and straightforward codec that
    represents an encoding-decoding scheme that is purely based upon only
    the encoding-decoding functions attached, and the fields of the
    `FeatureDescriptor` it is attached to, and doesn't track any parameters
    itself.
    
    Signed-off-by: Anant Thazhemadam <anant.thazhemadam@gmail.com>

--- src/codecs/codecs.jl
+++ src/codecs/codecs.jl
@@ -3,6 +3,9 @@ module Codec
 @enum EncodeOrDecode ENCODE DECODE
 export EncodeOrDecode
 
+include("simplecodec.jl")
+export SimpleCodec
+
 include("onehotonecold.jl")
 export OneHotOneCold
 
---
+++ src/codecs/simplecodec.jl
@@ -0,0 +1,17 @@
+using ..ChemistryFeaturization.AbstractType: AbstractCodec
+
+"""
+    SimpleCodec(encode_f, decode_f)
+
+SimpleCodec is a most simple, atomic (as in indivisible), and straightforward Codec.
+
+A SimpleCodec is characterized by only the encoding and decoding functions it has as fields.
+This means that the encoding-decoding scheme is entirely dependent only upon these functions, and the fields of the FeatureDescriptor to which this Codec is attached. No external parameters (such as number of bins, etc.) are required
+by the encoding-decoding implementations.
+
+This Codec is very useful when the encoding-decoding scheme that you want to define for a `FeatureDescriptor` is very simple.
+"""
+struct SimpleCodec <: AbstractCodec
+    encode_f::Function
+    decode_f::Function
+end
