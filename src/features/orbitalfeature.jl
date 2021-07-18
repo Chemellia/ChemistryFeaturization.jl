fd42c7e6c65e9b8ce5e9432393ca97353178d231
Anant Thazhemadam <anant.thazhemadam@gmail.com>
2021-07-19 03:20:35 +0530

    orbitalfeature: annotate, fix order of shells in lookup in defaultdecode
    * Add comments, and annotate `default_ofd_decode()`
    
    * Turns out, instead of having the electronic configuration for an
    element be represented in the order shells are actually filled, the
    lookup_table stores them in a more "readable" way, i.e., numerically
    sorted way.
    For instance, the valence shell configuration for "Fe" is `4s2.3d6` if
    you consider the order in which electrons are filled in the shells, but
    in the default lookup_table, the order is `3d6.4s2`.
    So, sort out the `shell_conf` first, before looking it up.
    
    Signed-off-by: Anant Thazhemadam <anant.thazhemadam@gmail.com>

--- src/features/orbitalfeature.jl
+++ src/features/orbitalfeature.jl
@@ -36,8 +36,9 @@ function default_ofd_encode(ofd::OrbitalFeatureDescriptor, a::AbstractAtoms)
     V = Vector{Int16}()
     col = 0
     for i in elements(a)
+        # X, Y are equivalent to I, V for a SparseVector
         X, Y = valence_shell_config(ofd.lookup_table, i)
-        col += 1
+        col += 1    # column number
 
         for i = 1:length(X)
             append!(I, X[i])
@@ -45,33 +46,39 @@ function default_ofd_encode(ofd::OrbitalFeatureDescriptor, a::AbstractAtoms)
             append!(V, Y[i])
         end
     end
-    sparse(I, J, V)
+    sparse(I, J, V) # create a SparseMatrix with the valence shell configuration of each element as a column vector
 end
 
 function default_ofd_decode(
     encoded_features::SparseArrays.AbstractSparseMatrixCSC{Tv,Ti},
 ) where {Tv,Ti}
     elements = String[]
-    rows = rowvals(encoded_features)
-    vals = nonzeros(encoded_features)
-    _, n = size(encoded_features)
-    shell_conf = ""
-    for j = 1:n
-        for i in nzrange(encoded_features, j)
-            row = rows[i]
+    rows = rowvals(encoded_features)  # the rows of the sparse matrix
+    vals = nonzeros(encoded_features) # the values in the sparse matrix
+    m, n = size(encoded_features) # 'dimensions' of the sparse matrix
+    # for each column vector (remember, a column vector is representative of an element's valence shell configuration)
+    for j = 1:n 
+        shell_conf = "" # the shell configuration for an element
+        for i in nzrange(encoded_features, j)   # for each valid value in a column
+            row = rows[i]  
             val = vals[i]
-            shell_conf = shell_conf * _indexorbital(row) * string(val) * "."
+            # find the configuration for each shell and concatenate it to the `shell_conf`
+            shell_conf = shell_conf * _indexorbital(row) * string(val) * "." 
         end
+        # `chop` the trailing '.', and rearrange order of the shells to match the format followed in the default `lookup_table`
+        shell_conf = join(sort(split(chop(shell_conf), '.'), by=first), '.')
+
+        # find the DataFrame that has the matching `shell_conf` as its Valence Shell Configuration, and get its `Symbol`
         element = (filter(
-            "Valence Shell Configuration" => x -> x == chop(shell_conf),
+            "Valence Shell Configuration" => x -> x == shell_conf,
             atom_data_df;
             view = true,
         )[
             !,
             :Symbol,
         ])[1]
+        # append the `Symbol` that was just extracted to the container meant for storing all the elements in the encoded Atoms object
         append!(elements, (element,))
-        shell_conf = ""
     end
     return elements
 end
