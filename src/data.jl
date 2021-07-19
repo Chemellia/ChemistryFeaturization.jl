374904ba6be071fd3b7616f86d832ebfb916c823
Anant Thazhemadam <anant.thazhemadam@gmail.com>
2021-07-19 23:36:43 +0530

    data.jl: remove const-ness for `atom_data_df` and `feature_info`
    Signed-off-by: Anant Thazhemadam <anant.thazhemadam@gmail.com>

--- src/data.jl
+++ src/data.jl
@@ -7,9 +7,9 @@ using JSON
 export atom_data_df, feature_info
 
 atom_data_path = joinpath(@__DIR__, "..", "data", "pymatgen_atom_data.csv")
-const atom_data_df = DataFrame(CSV.File(atom_data_path))
+atom_data_df = DataFrame(CSV.File(atom_data_path))
 
 feature_info_path = joinpath(@__DIR__, "..", "data", "feature_info.json")
-const feature_info = JSON.parsefile(feature_info_path)
+feature_info = JSON.parsefile(feature_info_path)
 
 end
