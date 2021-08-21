f6f5ab5ad722fef5c016b4a0d13ea59d00fc73a3
Anant Thazhemadam <anant.thazhemadam@gmail.com>
2021-08-22 03:40:36 +0530

    orbitalfeature:clean up default_ofd_decode & rename valence_shell_config
    * Clean up `default_ofd_decode`. Simplify the access/traversal logic.
    Move the same out of `default_ofd_decode` into a helper function
    `_econf_to_name` in `OrbitalFeatureUtils`.
    
    * Rename `valence_shell_config` to `_name_to_econf` so the parallel
    between it and `_econf_to_name` is evident and more obvious.
    
    * Format everything
    
    Signed-off-by: Anant Thazhemadam <anant.thazhemadam@gmail.com>

--- src/utils/elementfeature_utils.jl
+++ src/utils/elementfeature_utils.jl
@@ -123,7 +123,7 @@ function get_bins(
     logspaced::Bool = default_log(feature_name, lookup_table),
     categorical::Bool = default_categorical(feature_name, lookup_table),
 )
-    local bins , min_val , max_val
+    local bins, min_val, max_val
 
     if categorical
         if feature_name in categorical_feature_names
