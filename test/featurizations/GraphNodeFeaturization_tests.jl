c9bc4941ba5b1067b1b35c34f4e646095b10845d
Dhairya Gandhi <dhairya@juliacomputing.com>
2021-09-18 04:16:21 +0530

    add tests

--- test/featurizations/GraphNodeFeaturization_tests.jl
+++ test/featurizations/GraphNodeFeaturization_tests.jl
@@ -81,3 +81,4 @@ using ChemistryFeaturization.Featurization
     end
 
 end
+
--- test/utils/GraphBuilding_tests.jl
+++ test/utils/GraphBuilding_tests.jl
@@ -1,6 +1,8 @@
 using Test
 using ChemistryFeaturization.Utils.GraphBuilding
 using Xtals
+using Zygote, FiniteDifferences
+
 
 @testset "GraphBuilding" begin
     path1 = abspath(@__DIR__, "..", "test_data", "strucs", "mp-195.cif")
@@ -19,4 +21,28 @@ using Xtals
     adjc, elsc = build_graph(Crystal(path1))
     @test adjc == wm_true
     @test elsc == els_true
+end
+
+@testset "Graph Building AD tests" begin
+
+  function test_fd(i, j, dist)
+      fd = grad(forward_fdm(2,1),
+                (i,j,dist) -> sum(GraphBuilding.weights_cutoff(i,j,dist)),
+                i, j, dist)
+
+      gs = gradient(i, j, dist) do i, j, dist
+          sum(GraphBuilding.weights_cutoff(i, j, dist))
+      end
+
+      @test gs[1] == fill(nothing, length(i))
+      @test gs[2] == fill(nothing, length(j))
+      t = isapprox(gs[3], fd[3])
+      @show t
+      @test t
+  end
+
+  # test with non-overlapping indices
+  test_fd(collect(1:10), collect(1:10), Float64.(collect(1:10)))
+  # test with overlapping indices
+  test_fd(rand(1:10, 100), rand(1:10, 100), rand(100))
 end
