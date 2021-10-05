33b58f7595502afb81120d4bb272e988603e2121
Rachel Kurchin <rkurchin@cmu.edu>
2021-10-05 11:48:36 -0400

    Add files for codec tests, actual tests still to be written
    Really just committing this because I had it locally on my laptop and wanted it to get synced with my desktop...
    
    Signed-off-by: Rachel Kurchin <rkurchin@cmu.edu>

---
+++ test/codecs/codec_tests.jl
@@ -0,0 +1,7 @@
+@testset "Codecs" begin
+    codec_tests = ["OneHotOneCold_tests", "SimpleCodec_tests", "DirectCodec_tests"]
+    for t in codec_tests
+        tp = abspath(testdir, "codecs", "$(t).jl")
+        include(tp)
+    end
+end
