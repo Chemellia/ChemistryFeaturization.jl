ebadce1fa3945d5b2a4d6b300d10648d769cf1db
Rachel Kurchin <rkurchin@cmu.edu>
2021-10-06 19:04:51 -0400

    formatting
    Signed-off-by: Rachel Kurchin <rkurchin@cmu.edu>

--- test/codecs/DirectCodec_tests.jl
+++ test/codecs/DirectCodec_tests.jl
@@ -3,5 +3,5 @@
     @test encode(dc, 3) == 3
     @test encode(dc, [-2, 0]) == [-2, 0]
     @test decode(dc, 5) == 5
-    @test decode(dc, [1,2]) == [1,2]
+    @test decode(dc, [1, 2]) == [1, 2]
 end
