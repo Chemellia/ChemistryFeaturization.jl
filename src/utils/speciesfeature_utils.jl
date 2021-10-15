f4f0cbf460730e1f490c2f555cbd9ded191ebbad
Rachel Kurchin <rkurchin@cmu.edu>
2021-10-15 15:47:24 -0400

    add some more options to SFD utils
    Signed-off-by: Rachel Kurchin <rkurchin@cmu.edu>

--- src/utils/speciesfeature_utils.jl
+++ src/utils/speciesfeature_utils.jl
@@ -8,6 +8,20 @@ mg_elements = [d["Symbol"] for d in MolecularGraph.ATOMTABLE]
 
 export sfd_names_props
 sfd_names_props = Dict(
+    "hybridization" => Dict(
+        :A => GraphMol,
+        :compute_f => hybridization,
+        :categorical => true
+        :encodable_elements => mg_elements,
+        :possible_vals => [:sp3, :sp2, :sp, :none],
+    ),
+    "hydrogenconnected" => Dict(
+        :A => GraphMol,
+        :compute_f => hydrogenconnected,
+        :categorical => true
+        :encodable_elements => mg_elements,
+        :possible_vals => [0,1,2,3], # I'm hoping this is right...
+    ),
     "isaromatic" => Dict(
         :A => GraphMol,
         :compute_f => isaromatic,
@@ -15,6 +29,27 @@ sfd_names_props = Dict(
         :encodable_elements => mg_elements,
         :possible_vals => [true, false],
     ),
+    "isringatom" => Dict(
+        :A => GraphMol,
+        :compute_f => isringatom,
+        :categorical => true,
+        :encodable_elements => mg_elements,
+        :possible_vals => [true, false],
+    ),
+    "lonepair" => Dict(
+        :A => GraphMol,
+        :compute_f => lonepair,
+        :categorical => true,
+        :encodable_elements => mg_elements,
+        :possible_vals => [-1, 0, 1, 2, 3],
+    ),
+    "pielectron" => Dict(
+        :A => GraphMol,
+        :compute_f => pielectron,
+        :categorical => true,
+        :encodable_elements => mg_elements,
+        :possible_vals => [0, 1, 2], # also not certain this is correct
+    )
 )
 
 end
