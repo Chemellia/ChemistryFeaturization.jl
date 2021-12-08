a2528e25094580b6ac50ad2497c993f494e27f85
Dhairya Gandhi <dhairya@juliacomputing.com>
2021-12-08 17:37:59 +0530

    separate element and species feats

--- src/featurizations/weavefeaturization.jl
+++ src/featurizations/weavefeaturization.jl
@@ -7,26 +7,26 @@ using ..ChemistryFeaturization.Codec: DirectCodec
 using ..ChemistryFeaturization.Utils
 
 struct WeaveFeaturization <: AbstractFeaturization
+    element_features::Vector{<:AbstractAtomFeatureDescriptor}
     atom_features::Vector{<:AbstractAtomFeatureDescriptor}
     bond_features::Vector{<:BondFeatureDescriptor}
     pair_features::Vector{<:AbstractPairFeatureDescriptor}
 end
 
-function WeaveFeaturization(default_atom_feature_list = ["symbol",
-                                                         "degree",
-                                                         "implicithconnected",
-                                                         "charge",
-                                                         "radical_electrons",
-                                                         "hybridization",
-                                                         "isaromatic",
-                                                         "hydrogenconnected"],
-                            default_bond_feature_list = ["bondorder", "isaromaticbond", "isringbond"])
-  species_feats = intersect(keys(Utils.SpeciesFeatureUtils.sfd_names_props), default_atom_feature_list)
-  
-  atoms = SpeciesFeatureDescriptor.(species_feats)
-  bonds = BondFeatureDescriptor.(default_bond_feature_list)
+function WeaveFeaturization(element_feature_list = ["Atomic no"],
+                            species_feature_list = ["degree",
+                                                    "implicithconnected",
+                                                    "charge",
+                                                    "radical_electrons",
+                                                    "hybridization",
+                                                    "isaromatic",
+                                                    "hydrogenconnected"],
+                            bond_feature_list = ["bondorder", "isaromaticbond", "isringbond"])
+  elements = ElementFeatureDescriptor.(element_feature_list)  
+  species = SpeciesFeatureDescriptor.(species_feature_list)
+  bonds = BondFeatureDescriptor.(bond_feature_list)
   # pairs = PairFeatureDescriptor.(default_atom_feature_list)
-  WeaveFeaturization(atoms, bonds, bonds)
+  WeaveFeaturization(elements, species, bonds, bonds)
 end
 
 function encodable_elements(fzn::WeaveFeaturization)
@@ -37,6 +37,10 @@ end
 
 function atom_features(feat, mol; kw...)
   
+end
+
+function pair_features(feat, mol; kw...)
+
 end
 
 const DEEPCHEM_ATOM_SYMBOLS = [
@@ -98,9 +102,12 @@ end
 function encode(fzn::WeaveFeaturization, ag::AtomGraph; atom_feature_kwargs = (;),
                                                        bond_feature_kwargs = (;),
                                                        pair_feature_kwargs = (;))
-  af = mapreduce(x -> encode(x, ag, atom_feature_kwargs...), vcat, fzn.atom_features) 
+  sf = mapreduce(x -> encode(x, ag, atom_feature_kwargs...), vcat, fzn.atom_features)
+  ef = mapreduce(x -> encode(x, ag, atom_feature_kwargs...), vcat, fzn.element_features)
+  atom_and_elements = vcat(sf, ef)
   bf = cat(map(x -> encode(x, ag, bond_feature_kwargs...), fzn.bond_features)..., dims = 3)
   pf = cat(map(x -> encode(x, ag, pair_feature_kwargs...), fzn.pair_features)..., dims = 3)
-  FeaturizedWeave(af, bf, pf)
+  # Return FeaturizedAtoms here
+  FeaturizedWeave(atom_and_elements, bf, pf)
 end
 
