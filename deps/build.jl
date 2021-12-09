75df15f6f81bc793b0f871502a8665f36b7accc4
Rachel Kurchin <rkurchin@cmu.edu>
2021-12-09 11:45:33 -0500

    both mkl things inside if statement
    Signed-off-by: Rachel Kurchin <rkurchin@cmu.edu>

--- deps/build.jl
+++ deps/build.jl
@@ -5,5 +5,6 @@ Conda.add("rdkit", channel = "conda-forge")
 Conda.add("pymatgen", channel = "conda-forge")
 if "mkl" in Conda._installed_packages()
     Conda.rm("mkl")
+    Conda.add("mkl")
 end
-Conda.add("mkl")
+
