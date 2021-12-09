f5f67d32478ef137e42ed52a0193b58ee7049c4d
Rachel Kurchin <rkurchin@cmu.edu>
2021-12-09 11:28:44 -0500

    check if mkl is present before removing it
    Signed-off-by: Rachel Kurchin <rkurchin@cmu.edu>

--- deps/build.jl
+++ deps/build.jl
@@ -3,5 +3,7 @@ using Conda
 Conda.add("ase", channel = "conda-forge")
 Conda.add("rdkit", channel = "conda-forge")
 Conda.add("pymatgen", channel = "conda-forge")
-Conda.rm("mkl")
+if "mkl" in Conda._installed_packages()
+    Conda.rm("mkl")
+end
 Conda.add("mkl")
